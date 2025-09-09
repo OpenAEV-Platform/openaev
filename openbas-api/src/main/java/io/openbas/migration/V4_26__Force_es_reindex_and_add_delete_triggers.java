7b2fa6c0b34e66169e0967634d0ceb0c11e2e64d
Damien Goujard <damien.goujard@filigran.io>
2025-09-09 16:35:06 +0200

    [backend] migration

--- openbas-api/src/main/java/io/openbas/migration/V4_26__Force_es_reindex_and_add_delete_triggers.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_26__Force_es_reindex_and_add_delete_triggers.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_24__Force_es_reindex extends BaseJavaMigration {
+public class V4_26__Force_es_reindex_and_add_delete_triggers extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
