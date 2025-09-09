2982969ae9603013c97fecfe0cd801fbf9f41c1a
Damien Goujard <damien.goujard@filigran.io>
2025-09-09 16:26:10 +0200

    [backend] Marine comments

--- openbas-api/src/main/java/io/openbas/migration/V4_24__Force_es_reindex.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_24__Force_es_reindex.java
@@ -11,6 +11,59 @@ public class V4_24__Force_es_reindex extends BaseJavaMigration {
   @Override
   public void migrate(Context context) throws Exception {
     try (Statement statement = context.getConnection().createStatement()) {
+      // FIXME replace those triggers by another method, update the updated at attribute of the
+      // table
+      statement.executeUpdate(
+          """
+                  CREATE OR REPLACE FUNCTION update_asset_updated_at_after_delete_finding()
+                      RETURNS TRIGGER AS $$
+                  BEGIN
+                      UPDATE public.assets
+                      SET asset_updated_at = now()
+                      WHERE asset_id = OLD.asset_id;
+                      RETURN OLD;
+                  END;
+                  $$ LANGUAGE plpgsql;
+
+                  -- Trigger for AFTER DELETE
+                  CREATE TRIGGER after_delete_update_asset_updated_at
+                      AFTER DELETE ON public.findings_assets
+                      FOR EACH ROW EXECUTE FUNCTION update_asset_updated_at_after_delete_finding();
+              """);
+      statement.executeUpdate(
+          """
+                  CREATE OR REPLACE FUNCTION update_exercise_updated_at_after_delete_team()
+                      RETURNS TRIGGER AS $$
+                  BEGIN
+                      UPDATE public.exercises
+                      SET exercise_updated_at = now()
+                      WHERE exercise_id = OLD.exercise_id;
+                      RETURN OLD;
+                  END;
+                  $$ LANGUAGE plpgsql;
+
+                  -- Trigger for AFTER DELETE
+                  CREATE TRIGGER after_delete_update_exercise_updated_at
+                      AFTER DELETE ON public.exercises_teams
+                      FOR EACH ROW EXECUTE FUNCTION update_exercise_updated_at_after_delete_team();
+              """);
+      statement.executeUpdate(
+          """
+                  CREATE OR REPLACE FUNCTION update_scenario_updated_at_after_delete_team()
+                      RETURNS TRIGGER AS $$
+                  BEGIN
+                      UPDATE public.scenarios
+                      SET scenario_updated_at = now()
+                      WHERE scenario_id = OLD.scenario_id;
+                      RETURN OLD;
+                  END;
+                  $$ LANGUAGE plpgsql;
+
+                  -- Trigger for AFTER DELETE
+                  CREATE TRIGGER after_delete_update_scenario_updated_at
+                      AFTER DELETE ON public.scenarios_teams
+                      FOR EACH ROW EXECUTE FUNCTION update_scenario_updated_at_after_delete_team();
+              """);
       // re-index all in ES for the deletions
       statement.executeUpdate("DELETE FROM indexing_status;");
     }
