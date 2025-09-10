3e70e48dcf784eccd2f4922397637966b3b5adf2
Eva Ezzeribi <eva.ezzeribi@filigran.io>
2025-09-10 10:57:42 +0200

    [backend] - update migration V4_24 to V4_26 due to update from release

--- openbas-api/src/main/java/io/openbas/migration/V4_26__Modify_detection_remediation.java
+++ openbas-api/src/main/java/io/openbas/migration/V4_26__Modify_detection_remediation.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_24__Modify_detection_remediation extends BaseJavaMigration {
+public class V4_26__Modify_detection_remediation extends BaseJavaMigration {
   @Override
   public void migrate(Context context) throws Exception {
     try (Statement select = context.getConnection().createStatement()) {
