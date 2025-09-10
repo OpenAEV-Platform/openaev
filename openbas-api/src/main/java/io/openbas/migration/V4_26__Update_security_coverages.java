9ba6a450aee106902886563220a8f9db4629ff64
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-09-10 13:22:50 +0200

    [backend] Remove column not used in security coverages

---
+++ openbas-api/src/main/java/io/openbas/migration/V4_26__Update_security_coverages.java
@@ -0,0 +1,20 @@
+package io.openbas.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class V4_26__Update_security_coverages extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    Connection connection = context.getConnection();
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("ALTER TABLE security_coverages DROP COLUMN security_coverage_threat_context_ref;");
+    }
+  }
+}
