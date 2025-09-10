657188e461aea8b4f1b86bda18b6ff3df550ba4f
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-09-10 13:52:38 +0200

    [backend] fix query

--- openbas-model/src/main/java/io/openbas/database/repository/InjectorContractRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/InjectorContractRepository.java
@@ -47,7 +47,7 @@ public interface InjectorContractRepository
           "SELECT injcont FROM injectors_contracts injcont "
               + "LEFT JOIN injectors_contracts_vulnerabilities injconvuln ON injcont.injector_contract_id = injconvuln.injector_contract_id "
               + "LEFT JOIN cves cve ON injconvuln.vulnerability_id = cve.cve_id "
-              + "WHERE cve.external_id = :externalId "
+              + "WHERE cve.cve_external_id = :externalId "
               + "ORDER BY injcont.injector_contract_updated_at LIMIT :injectsPerVulnerability",
       nativeQuery = true)
   Set<InjectorContract> findInjectorContractsByVulnerabilityId(
