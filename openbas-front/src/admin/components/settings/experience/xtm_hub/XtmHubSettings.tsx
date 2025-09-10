c9f9531843a364d717f8eb0b77cc5a8e62f1dbe5
Valentin Stern <valentin.stern@filigran.io>
2025-09-10 17:09:11 +0200

    [frontend] feat: display registration info on experience page

--- openbas-front/src/admin/components/settings/experience/xtm_hub/XtmHubSettings.tsx
+++ openbas-front/src/admin/components/settings/experience/xtm_hub/XtmHubSettings.tsx
@@ -1,13 +1,17 @@
-import { List, ListItem, Paper, Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import type React from 'react';
 
 import { useFormatter } from '../../../../../components/i18n';
-import GradientButton from '../../../common/GradientButton';
-
+import useAuth from '../../../../../utils/hooks/useAuth';
+import XtmHubRegisteredSection from './XtmHubRegisteredSection';
+import XtmHubUnregisteredSection from './XtmHubUnregisteredSection';
 const XtmHubSettings: React.FC = () => {
   const { t } = useFormatter();
   const theme = useTheme();
+  const { settings } = useAuth();
+  const isXTMHubRegistered = settings?.xtm_hub_registration_status === 'registered' || settings?.xtm_hub_registration_status === 'lost_connectivity';
+
   return (
     <>
       <Typography
@@ -18,72 +22,16 @@ const XtmHubSettings: React.FC = () => {
       </Typography>
       <Paper
         style={{
-          padding: theme.spacing(2),
+          padding: theme.spacing(0, 2, 2),
           borderRadius: 4,
           flexGrow: 1,
         }}
         className="paper-for-grid"
         variant="outlined"
       >
-        <Typography variant="h6" style={{ marginBottom: theme.spacing(2) }}>
-          {t('Experiment valuable threat management resources in the XTM Hub')}
-        </Typography>
-        <Typography>{t('XTM Hub is a central forum to access resources, share tradecraft, and optimize the use of Filigran\'s products, fostering collaboration and empowering the community.')}</Typography>
-        <Typography>{t('By registering this platform into the hub, it will allow you to:')}</Typography>
-        <List
-          style={{
-            listStyleType: 'disc',
-            marginLeft: theme.spacing(2),
-          }}
-          dense
-        >
-          <ListItem style={{
-            display: 'list-item',
-            paddingLeft: 0,
-          }}
-          >
-            {t('Deploy in one-click threat management resources such as scenarios')}
-          </ListItem>
-          <ListItem style={{
-            display: 'list-item',
-            paddingLeft: 0,
-          }}
-          >
-            <span>
-              {t('Stay informed of new resources and key threat events with an exclusive news feed')}
-              <i>
-                {t(' (coming soon)')}
-              </i>
-            </span>
-          </ListItem>
-          <ListItem style={{
-            display: 'list-item',
-            paddingLeft: 0,
-          }}
-          >
-            <span>
-              {t('Monitor key metrics of the platform and health status')}
-              <i>
-                {t(' (coming soon)')}
-              </i>
-            </span>
-          </ListItem>
-        </List>
+        {!isXTMHubRegistered && <XtmHubUnregisteredSection />}
 
-        <GradientButton
-          variant="outlined"
-          component="a"
-          href="https://filigran.io/platforms/xtm-hub/"
-          target="_blank"
-          rel="noreferrer"
-          aria-label={t('Discover the Hub (external link)')}
-          style={{
-            marginTop: theme.spacing(1),
-            marginBottom: theme.spacing(1),
-          }}
-        >
-          {t('Discover the Hub')}
-        </GradientButton>
+        {isXTMHubRegistered && <XtmHubRegisteredSection />}
       </Paper>
     </>
   );
