f58578de1dd937ee7c7e182827207559bcb11de6
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-09-10 16:58:54 +0200

    [backend] Fix query

--- openbas-model/src/main/java/io/openbas/database/repository/InjectRepository.java
+++ openbas-model/src/main/java/io/openbas/database/repository/InjectRepository.java
@@ -358,13 +358,10 @@ public interface InjectRepository
   @Transactional
   @Query(
       value =
-          "DELETE i "
-              + "FROM injects i "
-              + "JOIN injectors_contracts ic "
-              + " ON i.inject_injector_contract_id = ic.injector_contract_id "
-              + "JOIN injectors_contracts_vulnerabilities icv "
-              + " ON ic.injector_contract_id = icv.injector_contract_id "
-              + "WHERE i.scenario_id = :scenarioId",
+          "DELETE FROM injects i "
+              + "INNER JOIN injectors_contracts ic ON i.inject_injector_contract_id = ic.injector_contract_id "
+              + "INNER JOIN injectors_contracts_vulnerabilities icv ON ic.injector_contract_id = icv.injector_contract_id "
+              + "WHERE i.inject_scenario = :scenarioId",
       nativeQuery = true)
   void deleteAllInjectsWithVulnerableContractsByScenarioId(@Param("scenarioId") String scenarioId);
 
@@ -372,13 +369,10 @@ public interface InjectRepository
   @Transactional
   @Query(
       value =
-          "DELETE i "
-              + "FROM injects i "
-              + "JOIN injectors_contracts ic "
-              + " ON i.inject_injector_contract_id = ic.injector_contract_id "
-              + "JOIN injectors_contracts_attack_patterns icap "
-              + " ON ic.injector_contract_id = icap.injector_contract_id "
-              + "WHERE i.scenario_id = :scenarioId",
+          "DELETE FROM injects i "
+              + "INNER JOIN injectors_contracts ic ON i.inject_injector_contract_id = ic.injector_contract_id "
+              + "INNER JOIN injectors_contracts_attack_patterns icap ON ic.injector_contract_id = icap.injector_contract_id "
+              + "WHERE i.inject_scenario = :scenarioId",
       nativeQuery = true)
   void deleteAllInjectsWithAttackPatternContractsByScenarioId(
       @Param("scenarioId") String scenarioId);
