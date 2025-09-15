4f109dd5a4aeedbbc8b8669cb3968c5b53b51bc0
Stephanya Casanova <stephanya.casanova@filigran.io>
2025-09-15 21:37:07 +0200

    [backend] Refact

--- openbas-api/src/main/java/io/openbas/rest/inject/service/InjectAssistantService.java
+++ openbas-api/src/main/java/io/openbas/rest/inject/service/InjectAssistantService.java
@@ -271,17 +271,17 @@ public class InjectAssistantService {
    * Generates injects for the given scenario and vulnerabilities
    *
    * @param scenario the scenario to which the injects belong
-   * @param vulnerabilityIds the set of Vulnerabilities (Cves) to generate injects for
+   * @param vulnerabilities the set of Vulnerabilities (Cves) to generate injects for
    * @param injectsPerVulnerability the number of injects to generate per Vulnerability
    * @return the list of created and saved injects
    */
   public Set<Inject> generateInjectsByVulnerabilities(
       Scenario scenario,
-      Set<Cve> vulnerabilityIds,
+      Set<Cve> vulnerabilities,
       int injectsPerVulnerability,
       InjectorContract contractForPlaceholder) {
     Set<Inject> injects =
-        vulnerabilityIds.stream()
+        vulnerabilities.stream()
             .flatMap(
                 vulnerability ->
                     buildInjectsByVulnerability(
