012899386e82c8e3314f29278bc3c89ccd40a7bd
Valentin Stern <valentin.stern@filigran.io>
2025-09-18 15:32:37 +0200

    [frontend] feat: refresh connectivity status when page is loaded

--- openbas-front/src/admin/components/settings/experience/xtm_hub/XtmHubSettings.tsx
+++ openbas-front/src/admin/components/settings/experience/xtm_hub/XtmHubSettings.tsx
@@ -1,11 +1,14 @@
 import { Paper, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import type React from 'react';
+import { useEffect, useRef } from 'react';
 
 import type { LoggedHelper } from '../../../../../actions/helper';
+import { refreshConnectivity } from '../../../../../actions/xtmhub/xtmhub-actions';
 import { useFormatter } from '../../../../../components/i18n';
 import { useHelper } from '../../../../../store';
 import type { PlatformSettings } from '../../../../../utils/api-types';
+import { useAppDispatch } from '../../../../../utils/hooks';
 import { Can } from '../../../../../utils/permissions/PermissionsProvider';
 import { ACTIONS, SUBJECTS } from '../../../../../utils/permissions/types';
 import XtmHubRegisteredSection from './XtmHubRegisteredSection';
@@ -15,6 +18,17 @@ const XtmHubSettings: React.FC = () => {
   const { t } = useFormatter();
   const theme = useTheme();
   const { settings }: { settings: PlatformSettings } = useHelper((helper: LoggedHelper) => ({ settings: helper.getPlatformSettings() }));
+  const dispatch = useAppDispatch();
+
+  const hasRun = useRef(false);
+  useEffect(() => {
+    if (!settings.xtm_hub_token || hasRun.current) {
+      return;
+    }
+
+    hasRun.current = true;
+    dispatch(refreshConnectivity());
+  }, []);
 
   const isXTMHubRegistered = settings?.xtm_hub_registration_status === 'registered' || settings?.xtm_hub_registration_status === 'lost_connectivity';
 
