ba0a993b7662bc013e56f99ab6959f8b35b8ec55
Valentin <6004961+Kroustille@users.noreply.github.com>
2025-09-19 09:24:25 +0200

    [backend] chore: move xtm hub file and add api url config (#4029)

--- openbas-api/src/test/java/io/openbas/config/XTMHubConfigTest.java
+++ openbas-api/src/test/java/io/openbas/config/XTMHubConfigTest.java
@@ -3,8 +3,8 @@ package io.openbas.config;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.openbas.IntegrationTest;
-import io.openbas.injectors.xtmhub.config.XTMHubConfig;
 import io.openbas.utils.mockConfig.WithMockXTMHubConfig;
+import io.openbas.xtmhub.config.XTMHubConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -27,6 +27,21 @@ public class XTMHubConfigTest extends IntegrationTest {
     public void shouldReturnEnabledStatusAndUrl() {
       assertThat(xtmHubConfig.getEnable()).isTrue();
       assertThat(xtmHubConfig.getUrl()).isEqualTo("https://hub.filigran.io");
+      assertThat(xtmHubConfig.getApiUrl()).isEqualTo("https://hub.filigran.io");
+    }
+  }
+
+  @Nested
+  @WithMockXTMHubConfig(url = "https://hub.filigran.io", override_api_url = "http://localhost:4002")
+  @DisplayName("When XTM Hub API URL is overridden")
+  public class withOverrideApiUrl {
+
+    @Autowired private XTMHubConfig xtmHubConfig;
+
+    @Test
+    @DisplayName("returns overridden API URL")
+    public void shouldReturnOverriddenApiUrl() {
+      assertThat(xtmHubConfig.getApiUrl()).isEqualTo("http://localhost:4002");
     }
   }
 
