250240c0173a54a3ed74ef885d2878b58a68c5f8
GabrielPEZÉ <gabriel.peze@filigran.io>
2025-09-26 17:13:38 +0200

    [frontend/backend] spotless

--- openaev-api/src/main/java/io/openaev/service/PlatformSettingsService.java
+++ openaev-api/src/main/java/io/openaev/service/PlatformSettingsService.java
@@ -198,13 +198,13 @@ public class PlatformSettingsService {
       platformSettings.setDefaultReplyTo(openAEVConfig.getDefaultReplyTo());
     }
     platformSettings.setSmtpServiceAvailable(
-            ofNullable(dbSettings.get(SMTP_SERVICE_AVAILABLE.key()))
-                    .map(Setting::getValue)
-                    .orElse(SMTP_SERVICE_AVAILABLE.defaultValue()));
+        ofNullable(dbSettings.get(SMTP_SERVICE_AVAILABLE.key()))
+            .map(Setting::getValue)
+            .orElse(SMTP_SERVICE_AVAILABLE.defaultValue()));
     platformSettings.setImapServiceAvailable(
-            ofNullable(dbSettings.get(IMAP_SERVICE_AVAILABLE.key()))
-                    .map(Setting::getValue)
-                    .orElse(IMAP_SERVICE_AVAILABLE.defaultValue()));
+        ofNullable(dbSettings.get(IMAP_SERVICE_AVAILABLE.key()))
+            .map(Setting::getValue)
+            .orElse(IMAP_SERVICE_AVAILABLE.defaultValue()));
 
     // Build authenticated user settings
     OpenAEVPrincipal user = currentUser();
