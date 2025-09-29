5b7000531ddb69df6ebdba42cbabffb526f8b1a1
Romuald Lemesle <romuald.lemesle@filigran.io>
2025-09-29 19:02:07 +0200

    [backend] Fix is ready on default value (#4098)

--- openbas-model/src/main/java/io/openbas/helper/InjectModelHelper.java
+++ openbas-model/src/main/java/io/openbas/helper/InjectModelHelper.java
@@ -297,8 +297,8 @@ public class InjectModelHelper {
           isSet = false;
           break;
         }
-        if (isTextOrTextarea(jsonField) && !isFieldValid(content, injectContractFields, key)) {
-          isSet = false;
+        if (isTextOrTextarea(jsonField)) {
+          isSet = isFieldValid(content, injectContractFields, key);
         } else if (content.get(key) == null
             || (content.get(key).isArray() && content.get(key).isEmpty())
             || (content.get(key).isObject() && content.get(key).isEmpty())
