82d793dfeb086375201f5636a6e0f7ca39b8a186
Carine Le Bas <carine.lebas@filigran.io>
2025-10-01 11:36:19 +0200

    [backend] fix: delete setting only if it exists

--- openbas-api/src/main/java/io/openbas/service/PlatformSettingsService.java
+++ openbas-api/src/main/java/io/openbas/service/PlatformSettingsService.java
@@ -596,14 +596,22 @@ public class PlatformSettingsService {
   public PlatformSettings deleteXTMHubRegistration() {
     Map<String, Setting> dbSettings = mapOfSettings(fromIterable(this.settingRepository.findAll()));
 
+    List<String> keys =
+        Arrays.asList(
+            XTM_HUB_TOKEN.key(),
+            XTM_HUB_REGISTRATION_DATE.key(),
+            XTM_HUB_REGISTRATION_STATUS.key(),
+            XTM_HUB_REGISTRATION_USER_ID.key(),
+            XTM_HUB_LAST_CONNECTIVITY_CHECK.key(),
+            XTM_HUB_SHOULD_SEND_CONNECTIVITY_EMAIL.key());
+
     List<Setting> toDelete = new ArrayList<>();
-    toDelete.add(dbSettings.get(XTM_HUB_TOKEN.key()));
-    toDelete.add(dbSettings.get(XTM_HUB_REGISTRATION_DATE.key()));
-    toDelete.add(dbSettings.get(XTM_HUB_REGISTRATION_STATUS.key()));
-    toDelete.add(dbSettings.get(XTM_HUB_REGISTRATION_USER_ID.key()));
-    toDelete.add(dbSettings.get(XTM_HUB_REGISTRATION_USER_NAME.key()));
-    toDelete.add(dbSettings.get(XTM_HUB_LAST_CONNECTIVITY_CHECK.key()));
-    toDelete.add(dbSettings.get(XTM_HUB_SHOULD_SEND_CONNECTIVITY_EMAIL.key()));
+    keys.forEach(
+        settingsKey -> {
+          if (dbSettings.containsKey(settingsKey)) {
+            toDelete.add(dbSettings.get(settingsKey));
+          }
+        });
 
     this.settingRepository.deleteAll(toDelete);
     return findSettings();
