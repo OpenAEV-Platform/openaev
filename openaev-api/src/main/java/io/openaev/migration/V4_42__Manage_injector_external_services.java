6838de0e3633da274bdd8a725c51da9f619087e0
GabrielPEZÉ <gabriel.peze@filigran.io>
2025-10-02 18:17:28 +0200

    [backend] increase migration file version

--- openaev-api/src/main/java/io/openaev/migration/V4_42__Manage_injector_external_services.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_42__Manage_injector_external_services.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_41__Manage_injector_external_services extends BaseJavaMigration {
+public class V4_42__Manage_injector_external_services extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
