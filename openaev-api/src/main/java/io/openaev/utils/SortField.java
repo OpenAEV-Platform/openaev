26d12c88ef93ff553815561e135174c60f27c48b
Marine LM <marine.lemezo@filigran.io>
2025-10-03 17:25:53 +0200

    [backend] remove useless file
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-api/src/main/java/io/openaev/utils/SortField.java
+++
@@ -1,9 +0,0 @@
-package io.openaev.utils;
-
-import javax.annotation.Nullable;
-import lombok.Builder;
-import org.springframework.data.domain.Sort;
-
-@Builder
-public record SortField(
-    String property, @Nullable String direction, @Nullable Sort.NullHandling nullHandling) {}
