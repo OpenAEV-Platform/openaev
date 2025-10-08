e5cec6932aed4010cfb9b7f2e3f0e0137ecb9d6e
GabrielPEZÉ <gabriel.peze@filigran.io>
2025-10-08 14:07:32 +0200

    [backend] Spotless

--- openaev-api/src/main/java/io/openaev/migration/V4_43__Create_Missing_Time_Range.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_43__Create_Missing_Time_Range.java
@@ -1,19 +1,18 @@
 package io.openaev.migration;
 
+import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
-import java.sql.Statement;
-
 @Component
 public class V4_43__Create_Missing_Time_Range extends BaseJavaMigration {
 
-    @Override
-    public void migrate(Context context) throws Exception {
-        try (Statement select = context.getConnection().createStatement()) {
-            select.execute(
-            """
+  @Override
+  public void migrate(Context context) throws Exception {
+    try (Statement select = context.getConnection().createStatement()) {
+      select.execute(
+          """
                     INSERT INTO custom_dashboards_parameters (custom_dashboards_parameter_id, custom_dashboard_id, custom_dashboards_parameter_name, custom_dashboards_parameter_type)
                     SELECT gen_random_uuid(), cd.custom_dashboard_id, 'Time Range', 'timeRange'
                     FROM custom_dashboards cd
@@ -22,6 +21,6 @@ public class V4_43__Create_Missing_Time_Range extends BaseJavaMigration {
                        AND cdp.custom_dashboards_parameter_type = 'timeRange'
                     WHERE cdp.custom_dashboards_parameter_type IS NULL
                 """);
-        }
     }
+  }
 }
