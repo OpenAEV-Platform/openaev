9cbc636a7ccd1e33b98c6c13c912a065c30fe59f
GabrielPEZÉ <gabriel.peze@filigran.io>
2025-10-09 11:42:27 +0200

    [backend] Fix PR feedback

--- openaev-api/src/main/java/io/openaev/migration/V4_43__Create_Missing_Time_Range.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_43__Create_Missing_Time_Range.java
@@ -21,6 +21,26 @@ public class V4_43__Create_Missing_Time_Range extends BaseJavaMigration {
                        AND cdp.custom_dashboards_parameter_type = 'timeRange'
                     WHERE cdp.custom_dashboards_parameter_type IS NULL
                 """);
+      select.execute(
+          """
+                      INSERT INTO custom_dashboards_parameters (custom_dashboards_parameter_id, custom_dashboard_id, custom_dashboards_parameter_name, custom_dashboards_parameter_type)
+                      SELECT gen_random_uuid(), cd.custom_dashboard_id, 'End date', 'endDate'
+                      FROM custom_dashboards cd
+                      LEFT JOIN custom_dashboards_parameters cdp
+                         ON cdp.custom_dashboard_id = cd.custom_dashboard_id
+                         AND cdp.custom_dashboards_parameter_type = 'endDate'
+                      WHERE cdp.custom_dashboards_parameter_type IS NULL
+                """);
+      select.execute(
+          """
+                      INSERT INTO custom_dashboards_parameters (custom_dashboards_parameter_id, custom_dashboard_id, custom_dashboards_parameter_name, custom_dashboards_parameter_type)
+                      SELECT gen_random_uuid(), cd.custom_dashboard_id, 'Start date', 'startDate'
+                      FROM custom_dashboards cd
+                      LEFT JOIN custom_dashboards_parameters cdp
+                         ON cdp.custom_dashboard_id = cd.custom_dashboard_id
+                         AND cdp.custom_dashboards_parameter_type = 'startDate'
+                      WHERE cdp.custom_dashboards_parameter_type IS NULL
+                """);
     }
   }
 }
