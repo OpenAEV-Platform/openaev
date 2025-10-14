b72b6fafefde22f5a5216033f0365696ef69ade6
johanah29 <johanah.lekeu@filigran.io>
2025-10-14 10:46:58 +0200

    correct drone
    Signed-off-by: johanah29 <johanah.lekeu@filigran.io>

--- openaev-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/ListWidget.tsx
+++ openaev-front/src/admin/components/workspaces/custom_dashboards/widgets/viz/list/ListWidget.tsx
@@ -93,7 +93,7 @@ const ListWidget = ({ widgetConfig, elements }: Props) => {
 
   return (
     <Box
-      style={{overflow: 'auto'}}
+      style={{ overflow: 'auto' }}
     >
       <MuiList>
         <MuiListItem
@@ -114,35 +114,35 @@ const ListWidget = ({ widgetConfig, elements }: Props) => {
         </MuiListItem>
         {elements.length === 0 && <div style={{ textAlign: 'center' }}>{t('No data to display')}</div>}
         {elements.map(e => (
-            <MuiListItem key={e.base_id} divider disablePadding secondaryAction={getSecondaryActionByBaseEntity(e)}>
-              <ListItemButton
-                key={e.base_id}
-                onClick={() => onListItemClick(e)}
-                classes={{ root: classes.item }}
-                className="noDrag"
-              >
-                <ListItemIcon>
-                  <DevicesOtherOutlined color="primary" />
-                </ListItemIcon>
-                <ListItemText
-                  primary={(
-                    <div style={bodyItemsStyles.bodyItems}>
-                      {widgetConfig.columns.map(col => (
-                        <div
-                          key={col}
-                          style={{
-                            ...bodyItemsStyles.bodyItem,
-                            ...stylesFromEntityType(elements)[col],
-                          }}
-                        >
-                          {elementsFromColumn(col, e)}
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                />
-              </ListItemButton>
-            </MuiListItem>
+          <MuiListItem key={e.base_id} divider disablePadding secondaryAction={getSecondaryActionByBaseEntity(e)}>
+            <ListItemButton
+              key={e.base_id}
+              onClick={() => onListItemClick(e)}
+              classes={{ root: classes.item }}
+              className="noDrag"
+            >
+              <ListItemIcon>
+                <DevicesOtherOutlined color="primary" />
+              </ListItemIcon>
+              <ListItemText
+                primary={(
+                  <div style={bodyItemsStyles.bodyItems}>
+                    {widgetConfig.columns.map(col => (
+                      <div
+                        key={col}
+                        style={{
+                         ...bodyItemsStyles.bodyItem,
+                         ...stylesFromEntityType(elements)[col],
+                        }}
+                      >
+                        {elementsFromColumn(col, e)}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              />
+            </ListItemButton>
+          </MuiListItem>
           ),
         )}
       </MuiList>
