91d3050d702b37b8f731a1d9cc3f10832d60a74a
GaetanSantucci <gaetan.santucci@filigran.io>
2025-10-20 11:48:20 +0200

    :bug: Rename migration file

--- openaev-api/src/main/java/io/openaev/migration/V4_44__Rename_cves_table_to_vulnerabilities.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_44__Rename_cves_table_to_vulnerabilities.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_42__Rename_cves_table_to_vulnerabilities extends BaseJavaMigration {
+public class V4_44__Rename_cves_table_to_vulnerabilities extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
