2407835bbc67dfbeee01289f2fefd4f0b9280139
Gabriel PEZÉ <gabriel.peze@filigran.io>
2025-10-28 09:17:54 +0100

    [backend] increase migration file version

--- openaev-api/src/main/java/io/openaev/migration/V4_46__Create_Dependencies_instead_of_fromStarterPack_Column.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_46__Create_Dependencies_instead_of_fromStarterPack_Column.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_44__Create_Dependencies_instead_of_fromStarterPack_Column
+public class V4_46__Create_Dependencies_instead_of_fromStarterPack_Column
     extends BaseJavaMigration {
 
   @Override
