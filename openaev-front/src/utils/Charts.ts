578bd876fd835d1f9e9cf276dcecf99b2b414353
Damien Goujard <damien.goujard@filigran.io>
2025-11-17 09:11:45 +0100

    [frontend] Fix apex charts

--- openaev-front/src/utils/Charts.ts
+++ openaev-front/src/utils/Charts.ts
@@ -494,7 +494,7 @@ export const horizontalBarsChartOptions = ({
     categories: categories ?? [],
     labels: {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error apexcharts typescript do not handle horizontal bar chart well
+      // apexcharts typescript do not handle horizontal bar chart well
       formatter: (value: number) => (xFormatter ? xFormatter(value) : value.toString()),
       style: { fontFamily: '"IBM Plex Sans", sans-serif' },
     },
