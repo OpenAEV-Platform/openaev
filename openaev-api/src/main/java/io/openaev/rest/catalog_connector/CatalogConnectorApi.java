f46bffdb5641d374d4d9f13977bd284804a93117
Camille Roux <camille.roux@filigran.io>
2025-11-24 16:05:01 +0100

    [frontend] Implement catalog

--- openaev-api/src/main/java/io/openaev/rest/catalog_connector/CatalogConnectorApi.java
+++ openaev-api/src/main/java/io/openaev/rest/catalog_connector/CatalogConnectorApi.java
@@ -19,9 +19,7 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
