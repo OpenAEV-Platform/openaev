3936d018a02b62cdef32d18ffc9918896a84ba74
Damien Goujard <damien.goujard@filigran.io>
2025-11-25 17:25:06 +0100

    [backend] Improve delete endpoint

--- openaev-model/src/main/java/io/openaev/database/repository/EndpointRepository.java
+++ openaev-model/src/main/java/io/openaev/database/repository/EndpointRepository.java
@@ -171,6 +171,15 @@ public interface EndpointRepository
       nativeQuery = true)
   void setUpdateDate(@Param("updateDate") Instant updateDate, @Param("id") String assetId);
 
+  // TODO this asset_id deleted -> f1a57c42-98e9-4538-926b-0be130c0af85
+  // Inject -> inject_assets?
+  // Widgets?
+  // ContractOutputType?
+  // Others?
+  @Modifying
+  @Query(value = "DELETE FROM assets WHERE asset_id = :assetId", nativeQuery = true)
+  void deleteById(@Param("assetId") @NotBlank String assetId);
+
   List<Endpoint> findDistinctByInjectsScenarioId(String scenarioId);
 
   List<Endpoint> findDistinctByInjectsScenarioIdAndIdIn(String scenarioId, List<String> ids);
