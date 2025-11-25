ad7fea0d7a551020e93e44f2db5aa9d879eaa575
Marine LM <marine.lemezo@filigran.io>
2025-11-25 11:07:03 +0100

    [backend] apply spotless
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-model/src/main/java/io/openaev/database/model/ConnectorInstanceLog.java
+++ openaev-model/src/main/java/io/openaev/database/model/ConnectorInstanceLog.java
@@ -3,7 +3,6 @@ package io.openaev.database.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import io.hypersistence.utils.hibernate.type.array.ListArrayType;
 import io.openaev.database.audit.ModelBaseListener;
 import io.openaev.helper.MonoIdDeserializer;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,7 +11,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UuidGenerator;
 
 @Getter
