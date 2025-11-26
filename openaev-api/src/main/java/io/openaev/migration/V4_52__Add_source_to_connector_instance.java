a7a2ee6d1fee43d1106491f0ed85abea727dbe5c
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-11-26 16:54:24 +0100

    add source to connector_instance
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

---
+++ openaev-api/src/main/java/io/openaev/migration/V4_52__Add_source_to_connector_instance.java
@@ -0,0 +1,23 @@
+package io.openaev.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.stereotype.Component;
+
+import java.sql.Statement;
+
+@Component
+public class V4_52__Add_source_to_connector_instance extends BaseJavaMigration {
+
+  @Override
+  public void migrate(Context context) throws Exception {
+    try (Statement select = context.getConnection().createStatement()) {
+      select.execute(
+          """
+        CREATE TYPE connector_instance_source AS ENUM ('PROPERTIES_MIGRATION', 'CATALOG_DEPLOYMENT', 'OTHER');
+        ALTER TABLE connector_instances ADD COLUMN connector_instance_source connector_instance_source NOT NULL DEFAULT 'OTHER'
+          );
+        """);
+    }
+  }
+}
