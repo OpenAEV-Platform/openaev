fa43fb3dfbf4c59ee3a7ebc48f255f78967a2839
Gaetan Santucci <gaetan.santucci@filigran.io>
2025-11-27 09:24:00 +0100

    :lipstick: Fix linter

--- openaev-front/src/admin/components/common/injects/Injects.tsx
+++ openaev-front/src/admin/components/common/injects/Injects.tsx
@@ -17,6 +17,7 @@ import SortHeadersComponentV2 from '../../../../components/common/queryable/sort
 import { useQueryableWithLocalStorage } from '../../../../components/common/queryable/useQueryableWithLocalStorage';
 import { useFormatter } from '../../../../components/i18n';
 import ItemBoolean from '../../../../components/ItemBoolean';
+import ItemDomains from '../../../../components/ItemDomains';
 import ItemTags from '../../../../components/ItemTags';
 import Loader from '../../../../components/Loader';
 import PaginatedListLoader from '../../../../components/PaginatedListLoader';
@@ -45,7 +46,6 @@ import InjectorContract from './InjectorContract';
 import InjectPopover from './InjectPopover';
 import InjectsListButtons from './InjectsListButtons';
 import UpdateInject from './UpdateInject';
-import ItemDomains from "../../../../components/ItemDomains";
 
 const useStyles = makeStyles()(() => ({
   disabled: {
@@ -139,18 +139,18 @@ const Injects: FunctionComponent<Props> = ({
       isSortable: true,
       value: (inject: InjectOutputType, _: InjectorContractConverted['convertedContent']) => <>{inject.inject_title}</>,
     },
-      {
-          field: 'inject_contract_domain',
-          label: t('domains'),
-          isSortable: true,
-          value: (inject: InjectOutputType, _: InjectorContractConverted['convertedContent']) => {
-              return inject.inject_injector_contract.injector_contract_domains && inject.inject_injector_contract.injector_contract_domains.length > 0
-                  ? (
-                      <ItemDomains domains={inject.inject_injector_contract.injector_contract_domains} variant="reduced-view" />
-                  )
-                  : <></>;
-          },
+    {
+      field: 'inject_contract_domain',
+      label: t('domains'),
+      isSortable: true,
+      value: (inject: InjectOutputType, _: InjectorContractConverted['convertedContent']) => {
+        return inject.inject_injector_contract.injector_contract_domains && inject.inject_injector_contract.injector_contract_domains.length > 0
+          ? (
+              <ItemDomains domains={inject.inject_injector_contract.injector_contract_domains} variant="reduced-view" />
+            )
+          : <></>;
       },
+    },
     {
       field: 'inject_depends_duration',
       label: 'Trigger',
