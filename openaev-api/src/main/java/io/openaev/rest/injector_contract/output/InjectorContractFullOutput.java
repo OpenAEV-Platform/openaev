bf465092146fcc22861392e5293eb0b8eefb85da
johanah29 <johanah.lekeu@filigran.io>
2025-12-01 09:21:03 +0100

    Resolve drone
    Signed-off-by: johanah29 <johanah.lekeu@filigran.io>

--- openaev-api/src/main/java/io/openaev/rest/injector_contract/output/InjectorContractFullOutput.java
+++ openaev-api/src/main/java/io/openaev/rest/injector_contract/output/InjectorContractFullOutput.java
@@ -8,8 +8,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import java.time.Instant;
 import java.util.*;
-
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
