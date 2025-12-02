1a04a730ad1bfe3fee4d3fc01c63b15d92ae1455
Gaetan Santucci <gaetan.santucci@outlook.com>
2025-12-02 21:18:25 +0100

    WIP

--- openaev-front/src/admin/components/integrations/injectors/injector_contracts/InjectorContractCustomForm.js
+++ openaev-front/src/admin/components/integrations/injectors/injector_contracts/InjectorContractCustomForm.js
@@ -1,9 +1,17 @@
-import { Button, GridLegacy, Switch, TextField as MUITextField, Typography } from '@mui/material';
+import {
+    Autocomplete, Box,
+    Button,
+    GridLegacy,
+    Switch,
+    TextField,
+    TextField as MUITextField,
+    Typography
+} from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import * as PropTypes from 'prop-types';
 import * as R from 'ramda';
 import { useState } from 'react';
-import { Form } from 'react-final-form';
+import {Field, Form} from 'react-final-form';
 
 import CKEditor from '../../../../../components/CKEditor';
 import OldTextField from '../../../../../components/fields/OldTextField';
@@ -26,6 +34,9 @@ const InjectorContractForm = (props) => {
     return errors;
   };
   const contract = JSON.parse(contractTemplate.injector_contract_content);
+    console.log('contract', contract);
+    const filteredDomains = contractTemplate.injector_contract_domains.filter(d => d.domain_name !== 'To classify');
+    console.log('filteredDomains', filteredDomains);
   const renderField = (field) => {
     switch (field.type) {
       case 'textarea':
@@ -111,7 +122,45 @@ const InjectorContractForm = (props) => {
             style={{ marginTop: 20 }}
             useExternalId={!editing}
           />
-          {contract.fields.map((field) => {
+            <Field
+                name="injector_contract_domains"
+            >
+                {({ input, meta }) => (
+                    <Autocomplete
+                        size="small"
+                        multiple
+                        options={filteredDomains}
+                        getOptionLabel={option => option.domain_name}
+                        isOptionEqualToValue={(option, val) => option.domain_id === val.domain_id}
+                        disableClearable={false}
+                        openOnFocus
+                        autoHighlight
+                        noOptionsText="No available options"
+                        value={Array.isArray(input.value) ? input.value : []}
+                        onChange={(_event, selectedOptions) => {
+                            input.onChange(selectedOptions);
+                        }}
+                        renderInput={params => (
+                            <TextField
+                                {...params}
+                                label={t('Domains')}
+                                variant="standard"
+                                size="small"
+                                fullWidth
+                                error={meta.error && meta.touched}
+                                helperText={meta.touched && meta.error ? meta.error : null}
+                            />
+                        )}
+                        renderOption={(props, option) => (
+                            <Box component="li" {...props} key={option.domain_id}>
+                                {option.domain_name}
+                            </Box>
+                        )}
+                    />
+                )}
+            </Field>
+
+            {contract.fields.map((field) => {
             return (
               <div
                 key={field.key}
