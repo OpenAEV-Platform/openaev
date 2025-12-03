796aa71c82b197cbd49c5369a0ccb313660a92c4
Eva Ezzeribi <eva.ezzeribi@filigran.io>
2025-12-03 11:18:26 +0100

    [backend] update model

--- openaev-model/src/main/java/io/openaev/database/model/Edge.java
+++ openaev-model/src/main/java/io/openaev/database/model/Edge.java
@@ -2,10 +2,7 @@ package io.openaev.database.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.hypersistence.utils.hibernate.type.json.JsonType;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -21,35 +18,24 @@ import java.util.Objects;
 @Table(name = "edges")
 public class Edge {
 
-  @EmbeddedId
-  @JsonProperty("dependency_relationship")
-  private InjectDependencyId compositeId = new InjectDependencyId();
+  @Id
+  @Column(name = "edge_id")
+  private String id;
 
-  @Column(name = "dependency_condition")
-  @JsonProperty("dependency_condition")
-  @Type(JsonType.class)
-  private InjectDependencyConditions.InjectDependencyCondition injectDependencyCondition;
+  @Column(name = "step_parent_id")
+  private String parentId;
+
+  @Column(name = "step_children_id")
+  private String stepChildrenId;
 
   @CreationTimestamp
-  @Column(name = "dependency_created_at")
+  @Column(name = "edge_created_at")
   @JsonProperty("dependency_created_at")
   private Instant creationDate;
 
   @UpdateTimestamp
-  @Column(name = "dependency_updated_at")
+  @Column(name = "edge_updated_at")
   @JsonProperty("dependency_updated_at")
   private Instant updateDate;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Edge that = (Edge) o;
-    return compositeId.equals(that.compositeId);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(compositeId);
-  }
 }
