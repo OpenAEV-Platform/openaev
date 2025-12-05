7a8750afa7c62273b8b3e5dfb1c5fe9361ed1704
Eva Ezzeribi <eva.ezzeribi@filigran.io>
2025-12-05 17:01:43 +0100

    Changes by Stephanya

--- openaev-api/src/main/java/io/openaev/migration/V4_52__Add_workflow_step_entities.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_52__Add_workflow_step_entities.java
@@ -14,8 +14,32 @@ public class V4_52__Add_workflow_step_entities extends BaseJavaMigration {
 
       select.execute(
           """
-        CREATE TYPE step_type AS ENUM ('INJECT_EXECUTION', 'TIMESTAMP', 'CONDITION');
-        CREATE TYPE step_status AS ENUM ('WAIT', 'RUN', 'END');
+        CREATE TYPE workflow_status AS ENUM ('TEMPLATE', 'STOP', 'RUN', 'END');
+        CREATE TABLE workflows (
+            workflow_id VARCHAR(255) NOT NULL CONSTRAINT workflow_pkey PRIMARY KEY,
+            workflow_status workflow_status,
+            workflow_version INTEGER NOT NULL,
+            workflow_is_edited BOOLEAN DEFAULT false,
+            workflow_simulation_id VARCHAR(255) NOT NULL REFERENCES exercises(exercise_id) ON DELETE CASCADE,
+            workflow_created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+            workflow_updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+          );
+        CREATE UNIQUE INDEX IF NOT EXISTS uk_workflow_template
+            ON workflows (workflow_id, workflow_simulation_id)
+            WHERE workflow_status = 'TEMPLATE';
+         """);
+      select.execute(
+          """
+      BEGIN IF NOT EXISTS (SELECT 1 FROM pg_type WHERE tyname = 'step_type' ) then
+              CREATE TYPE step_type AS ENUM ('INJECT_EXECUTION', 'TIMESTAMP', 'CONDITION');
+        END IF;
+       END;
+      
+      BEGIN IF NOT EXISTS (SELECT 1 FROM pg_type WHERE tyname = 'step_status' ) then
+        CREATE TYPE step_status AS ENUM ('TEMPLATE', 'WAIT', 'RUN', 'END');
+        END IF;
+       END;
+      
         CREATE TYPE step_action_class AS ENUM ('INJECT_EXECUTION');
         CREATE TABLE steps (
             step_id VARCHAR(255) NOT NULL CONSTRAINT step_pkey PRIMARY KEY,
@@ -23,21 +47,41 @@ public class V4_52__Add_workflow_step_entities extends BaseJavaMigration {
             step_output JSONB,
             step_input JSONB,
             step_data JSONB,
-            step_limit_execution INTEGER,
+            step_limit_execution INTEGER NOT NULL,
             step_status step_status,
             step_order INTEGER CHECK (step_order >= 1),
+            step_workflow_id VARCHAR(255) NOT NULL REFERENCES workflows(workflow_id) ON DELETE CASCADE,
             step_created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
             step_updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
           );
-        """);
+      """);
       select.execute(
           """
         CREATE TABLE edges (
             edge_id VARCHAR(255) NOT NULL CONSTRAINT edge_pkey PRIMARY KEY,
-            step_parent_id VARCHAR(255) NOT NULL UNIQUE REFERENCES steps(step_id),
-            step_children_id VARCHAR(255) NOT NULL UNIQUE REFERENCES steps(step_id),
+            step_parent_id VARCHAR(255) NOT NULL UNIQUE REFERENCES steps(step_id) ON DELETE CASCADE,
+            step_children_id VARCHAR(255) NOT NULL UNIQUE REFERENCES steps(step_id) ON DELETE CASCADE,
+            edge_template_id VARCHAR(255) NOT NULL REFERENCES edges(edge_id) ON DELETE CASCADE,
+            edge_workflow_id VARCHAR(255) NOT NULL REFERENCES workflows(workflow_id) ON DELETE CASCADE,
             edge_created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
             edge_updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+          );
+        """);
+      select.execute(
+          """
+
+        CREATE TYPE step_status AS ENUM ('AND', 'OR', 'EQ', 'NEQ', 'IS_NULL', 'IS_NOT_NULL', 'GT', 'GTE', 'LT', 'LTE', 'IN', 'NIN','AFTER','BEFORE');
+        CREATE TABLE conditions (
+            condition_id VARCHAR(255) NOT NULL CONSTRAINT condition_pkey PRIMARY KEY,
+            condition_key VARCHAR(255) NOT NULL,
+            condition_value VARCHAR(255) NOT NULL,
+            condition_type VARCHAR(255) NOT NULL,
+            condition_workflow_id VARCHAR(255) NOT NULL REFERENCES workflows(workflow_id) ON DELETE CASCADE,
+            condition_edge_template_id VARCHAR(255) NOT NULL REFERENCES edges(edge_id) ON DELETE CASCADE,
+            condition_edge_execution_id VARCHAR(255) NOT NULL REFERENCES edges(edge_id) ON DELETE CASCADE,
+            condition_created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+            condition_updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+            UNIQUE (condition_edge_execution_id, condition_key, condition_value, condition_type)
           );
         """);
     }
