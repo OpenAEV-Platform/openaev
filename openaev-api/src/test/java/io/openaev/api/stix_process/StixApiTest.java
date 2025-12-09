a71360742c82b14e28c4ef8894da4f179d0e8c2a
savacano28 <stephanya.casanova@filigran.io>
2025-12-09 17:32:44 +0100

    [backend] fix(securitycoverage): spotless

--- openaev-api/src/test/java/io/openaev/api/stix_process/StixApiTest.java
+++ openaev-api/src/test/java/io/openaev/api/stix_process/StixApiTest.java
@@ -454,19 +454,20 @@ class StixApiTest extends IntegrationTest {
       entityManager.flush();
       entityManager.clear();
 
-        String securityCoverageModify = stixSecurityCoverage.replace("2025-08-04T14:00:00Z", "2025-12-20T14:00:00Z");
+      String modifiedSecurityCoverage =
+          stixSecurityCoverage.replace("2025-08-04T14:00:00Z", "2025-12-20T14:00:00Z");
       // Push same stix in order to check the number of created injects
       String updatedResponse =
           mvc.perform(
                   post(STIX_URI + "/process-bundle")
                       .contentType(MediaType.APPLICATION_JSON)
-                      .content(securityCoverageModify))
+                      .content(modifiedSecurityCoverage))
               .andExpect(status().isOk())
               .andReturn()
               .getResponse()
               .getContentAsString();
 
-        scenarioId = JsonPath.read(updatedResponse, "$.scenarioId");
+      scenarioId = JsonPath.read(updatedResponse, "$.scenarioId");
       Scenario updatedScenario = scenarioRepository.findById(scenarioId).orElseThrow();
       assertThat(updatedScenario.getName())
           .isEqualTo("Security Coverage Q3 2025 - Threat Report XYZ");
@@ -475,51 +476,49 @@ class StixApiTest extends IntegrationTest {
       assertThat(injects).hasSize(4);
     }
 
-      @Test
-      @DisplayName(
-              "Should throw obsolete error from stix bundle")
-      void shouldThrowObsoleteStixBundle()
-              throws Exception {
-          String createdResponse =
-                  mvc.perform(
-                                  post(STIX_URI + "/process-bundle")
-                                          .contentType(MediaType.APPLICATION_JSON)
-                                          .content(stixSecurityCoverage))
-                          .andExpect(status().isOk())
-                          .andReturn()
-                          .getResponse()
-                          .getContentAsString();
+    @Test
+    @DisplayName("Should throw obsolete error from stix bundle")
+    void shouldThrowObsoleteStixBundle() throws Exception {
+      String createdResponse =
+          mvc.perform(
+                  post(STIX_URI + "/process-bundle")
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .content(stixSecurityCoverage))
+              .andExpect(status().isOk())
+              .andReturn()
+              .getResponse()
+              .getContentAsString();
 
-          String scenarioId = JsonPath.read(createdResponse, "$.scenarioId");
-          Scenario createdScenario = scenarioRepository.findById(scenarioId).orElseThrow();
-          assertThat(createdScenario.getName())
-                  .isEqualTo("Security Coverage Q3 2025 - Threat Report XYZ");
+      String scenarioId = JsonPath.read(createdResponse, "$.scenarioId");
+      Scenario createdScenario = scenarioRepository.findById(scenarioId).orElseThrow();
+      assertThat(createdScenario.getName())
+          .isEqualTo("Security Coverage Q3 2025 - Threat Report XYZ");
 
-          Set<Inject> injects = injectRepository.findByScenarioId(createdScenario.getId());
-          assertThat(injects).hasSize(4);
+      Set<Inject> injects = injectRepository.findByScenarioId(createdScenario.getId());
+      assertThat(injects).hasSize(4);
 
-          entityManager.flush();
-          entityManager.clear();
+      entityManager.flush();
+      entityManager.clear();
 
-          // Push same stix in order to check the number of created injects
-          String updatedResponse =
-                  mvc.perform(
-                                  post(STIX_URI + "/process-bundle")
-                                          .contentType(MediaType.APPLICATION_JSON)
-                                          .content(stixSecurityCoverage))
-                          .andExpect(status().isBadRequest())
-                          .andReturn()
-                          .getResponse()
-                          .getContentAsString();
+      // Push same stix in order to check the number of created injects
+      String updatedResponse =
+          mvc.perform(
+                  post(STIX_URI + "/process-bundle")
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .content(stixSecurityCoverage))
+              .andExpect(status().isBadRequest())
+              .andReturn()
+              .getResponse()
+              .getContentAsString();
 
-          scenarioId = JsonPath.read(updatedResponse, "$.scenarioId");
-          Scenario updatedScenario = scenarioRepository.findById(scenarioId).orElseThrow();
-          assertThat(updatedScenario.getName())
-                  .isEqualTo("Security Coverage Q3 2025 - Threat Report XYZ");
-          // ASSERT injects for updated stix
-          injects = injectRepository.findByScenarioId(updatedScenario.getId());
-          assertThat(injects).hasSize(4);
-      }
+      scenarioId = JsonPath.read(updatedResponse, "$.scenarioId");
+      Scenario updatedScenario = scenarioRepository.findById(scenarioId).orElseThrow();
+      assertThat(updatedScenario.getName())
+          .isEqualTo("Security Coverage Q3 2025 - Threat Report XYZ");
+      // ASSERT injects for updated stix
+      injects = injectRepository.findByScenarioId(updatedScenario.getId());
+      assertThat(injects).hasSize(4);
+    }
 
     @Test
     @DisplayName(
