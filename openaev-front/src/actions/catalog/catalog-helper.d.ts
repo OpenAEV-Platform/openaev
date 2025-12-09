4b8d1e632ba887cf4c8f02d7ba8db2f9361bd9c8
Camille Roux <camille.roux@filigran.io>
2025-12-09 15:00:34 +0100

    [frontend] fix lint

--- openaev-front/src/actions/catalog/catalog-helper.d.ts
+++ openaev-front/src/actions/catalog/catalog-helper.d.ts
@@ -1,1 +1,6 @@
-import { type CatalogConnectorOutput } from '../../utils/api-types';export interface CatalogConnectorsHelper {  getCatalogConnectors: () => CatalogConnectorOutput[];  getCatalogConnector: (connectorId: string) => CatalogConnectorOutput;}
+import { type CatalogConnectorOutput } from '../../utils/api-types';
+
+export interface CatalogConnectorsHelper {
+  getCatalogConnectors: () => CatalogConnectorOutput[];
+  getCatalogConnector: (connectorId: string) => CatalogConnectorOutput;
+}
