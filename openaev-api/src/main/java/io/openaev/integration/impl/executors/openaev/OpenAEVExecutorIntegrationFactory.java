ba3713c4b3742a5db4dee9286f1f3c3348e78f88
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-12-16 17:40:20 +0100

    restore openaev executor integration by default
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-api/src/main/java/io/openaev/integration/impl/executors/openaev/OpenAEVExecutorIntegrationFactory.java
+++ openaev-api/src/main/java/io/openaev/integration/impl/executors/openaev/OpenAEVExecutorIntegrationFactory.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("!test")
 public class OpenAEVExecutorIntegrationFactory extends IntegrationFactory {
   private final ExecutorService executorService;
   private final ComponentRequestEngine componentRequestEngine;
