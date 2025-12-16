ea279559f6d0f4c735f7c71e511741de82c3fd5e
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-12-16 17:40:53 +0100

    Revert "restore openaev executor integration by default"
    This reverts commit ba3713c4b3742a5db4dee9286f1f3c3348e78f88.

--- openaev-api/src/main/java/io/openaev/integration/impl/executors/openaev/OpenAEVExecutorIntegrationFactory.java
+++ openaev-api/src/main/java/io/openaev/integration/impl/executors/openaev/OpenAEVExecutorIntegrationFactory.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("!test")
 public class OpenAEVExecutorIntegrationFactory extends IntegrationFactory {
   private final ExecutorService executorService;
   private final ComponentRequestEngine componentRequestEngine;
