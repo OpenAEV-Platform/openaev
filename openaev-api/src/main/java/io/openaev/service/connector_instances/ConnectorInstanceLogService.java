f7378a8c340c3399ee9d2b2274f7f1cc188218ab
Marine LM <marine.lemezo@filigran.io>
2025-12-23 11:40:19 +0100

    [backend] feat: increase log size
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-api/src/main/java/io/openaev/service/connector_instances/ConnectorInstanceLogService.java
+++ openaev-api/src/main/java/io/openaev/service/connector_instances/ConnectorInstanceLogService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ConnectorInstanceLogService {
-  public static final long LOG_SIZE_LIMIT = 3L;
+  public static final long LOG_SIZE_LIMIT = 1000L;
   private final ConnectorInstanceLogRepository connectorInstanceLogRepository;
 
   private void cleanupExcessLogs(String connectorInstanceId) {
