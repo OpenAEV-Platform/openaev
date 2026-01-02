f38e724e256e4ca0edf2df2f7b9c2b7851134eff
Gabriel PEZÉ <gabriel.peze@filigran.io>
2026-01-02 17:43:04 +0100

    [backend] feat(stix): fix wrong migration modification (#3509)

--- openaev-api/src/main/java/io/openaev/migration/V4_59__Alter_operators_for_multiple_paths.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_59__Alter_operators_for_multiple_paths.java
@@ -6,11 +6,15 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 <<<<<<<< HEAD:openaev-api/src/main/java/io/openaev/migration/V4_59__Alter_operators_for_multiple_paths.java
+<<<<<<<< HEAD:openaev-api/src/main/java/io/openaev/migration/V4_59__Alter_operators_for_multiple_paths.java
 @Component
 public class V4_59__Alter_operators_for_multiple_paths extends BaseJavaMigration {
 ========
 public class V4_60__Alter_operators_for_multiple_paths extends BaseJavaMigration {
 >>>>>>>> 4995ac890 ([backend] feat(stix): fix tests (#3509)):openaev-api/src/main/java/io/openaev/migration/V4_60__Alter_operators_for_multiple_paths.java
+========
+public class V4_57__Alter_operators_for_multiple_paths extends BaseJavaMigration {
+>>>>>>>> 5c62d13d2 ([backend] feat(stix): fix wrong migration modification (#3509)):openaev-api/src/main/java/io/openaev/migration/V4_57__Alter_operators_for_multiple_paths.java
   @Override
   public void migrate(Context context) throws Exception {
     try (Statement stmt = context.getConnection().createStatement()) {
--- openaev-api/src/main/java/io/openaev/migration/V4_60__Add_indicators_column_to_security_coverage.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_60__Add_indicators_column_to_security_coverage.java
@@ -4,7 +4,7 @@ import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-public class V4_57__Add_indicators_column_to_security_coverage extends BaseJavaMigration {
+public class V4_60__Add_indicators_column_to_security_coverage extends BaseJavaMigration {
   @Override
   public void migrate(Context context) throws Exception {
     try (Statement stmt = context.getConnection().createStatement()) {
