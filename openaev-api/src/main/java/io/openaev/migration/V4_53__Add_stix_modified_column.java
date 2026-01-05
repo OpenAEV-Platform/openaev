98ba52b68498fe5030866fef2653497fa14ed182
savacano28 <stephanya.casanova@filigran.io>
2026-01-05 14:10:03 +0100

    [backend] fix(securitycoverage): add stix modified column

--- openaev-api/src/main/java/io/openaev/migration/V4_53__Add_stix_modified_column.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_53__Add_stix_modified_column.java
@@ -1,10 +1,9 @@
 package io.openaev.migration;
 
+import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
-
-import java.sql.Statement;
 
 @Component
 public class V4_53__Add_stix_modified_column extends BaseJavaMigration {
@@ -14,21 +13,13 @@ public class V4_53__Add_stix_modified_column extends BaseJavaMigration {
     try (Statement statement = context.getConnection().createStatement()) {
       statement.execute(
           """
-                ALTER TABLE security_coverages
-                ADD COLUMN security_coverage_stix_modified TIMESTAMPTZ;
+              ALTER TABLE security_coverages ADD COLUMN security_coverage_stix_modified TIMESTAMPTZ;
                 """);
 
       statement.execute(
           """
-                UPDATE security_coverages
-                SET security_coverage_stix_modified = security_coverage_updated_at
+                UPDATE security_coverages SET security_coverage_stix_modified = security_coverage_updated_at
                 WHERE security_coverage_stix_modified IS NULL;
-                """);
-
-      statement.execute(
-          """
-                CREATE UNIQUE INDEX idx_security_coverage_bundle_hash_md5
-                ON security_coverages(security_coverage_bundle_hash_md5);
                 """);
     }
   }
