d736bb53ccd5920ad6aa1dc858ac88a0d63fc412
Marine LM <marine.lemezo@filigran.io>
2026-01-05 11:57:23 +0100

    [backend] fix: apply spotless
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-api/src/main/java/io/openaev/rest/executor/form/ExecutorOutput.java
+++ openaev-api/src/main/java/io/openaev/rest/executor/form/ExecutorOutput.java
@@ -1,14 +1,11 @@
 package io.openaev.rest.executor.form;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.hypersistence.utils.hibernate.type.array.StringArrayType;
 import io.openaev.rest.catalog_connector.dto.CatalogConnectorSimpleOutput;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import java.time.Instant;
 import lombok.Builder;
-import org.hibernate.annotations.Type;
 
 @Builder
 @Schema(description = "Executor output")
