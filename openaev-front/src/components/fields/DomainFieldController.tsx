caffeb654cdd46ac291bf9f7dd1400b762476ddf
Gaetan Santucci <gaetan.santucci@outlook.com>
2026-01-14 12:11:28 +0100

    add method to remove to classify if another domains is selected in updat

--- openaev-front/src/components/fields/DomainFieldController.tsx
+++ openaev-front/src/components/fields/DomainFieldController.tsx
@@ -27,21 +27,23 @@ const DomainFieldController = ({
     <Controller
       name={name}
       control={control}
-      render={({ field: { onChange, value }, fieldState: { error } }) => (
-        <>
+      render={({ field: { onChange, value }, fieldState: { error } }) => {
+        const currentValues = Array.isArray(value) ? value : [];
+
+        return (
           <Autocomplete
             size="small"
             multiple
             options={filteredDomains}
             getOptionLabel={option => option.domain_name}
             isOptionEqualToValue={(option, val) => option.domain_id === val.domain_id}
-            disableClearable={false}
-            openOnFocus
-            autoHighlight
-            noOptionsText="No available options"
-            value={Array.isArray(value) ? value : []}
+            value={currentValues}
             onChange={(_event, selectedOptions) => {
-              onChange(selectedOptions);
+              const finalOptions = selectedOptions.length > 1
+                ? selectedOptions.filter(d => d.domain_name !== 'To classify')
+                : selectedOptions;
+
+              onChange(finalOptions);
             }}
             renderInput={params => (
               <TextField
@@ -60,8 +62,8 @@ const DomainFieldController = ({
               </Box>
             )}
           />
-        </>
-      )}
+        );
+      }}
     />
   );
 };
