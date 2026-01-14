e8677f1707771091d3e889f3b51f07501068eaa0
Gael Leblan <gael.leblan@filigran.io>
2026-01-14 23:12:31 +0100

    [backend] feat(catalog): encryption of sensitive values in database (#4605)

--- openaev-front/src/actions/executors/executor-action.ts
+++ openaev-front/src/actions/executors/executor-action.ts
@@ -2,6 +2,7 @@ import type { Dispatch } from 'redux';
 
 import { getReferential, simpleCall } from '../../utils/Action';
 import * as schema from '../Schema';
+import { fetchConnectorInstanceConfigurations } from "../connector_instances/connector-instance-actions";
 
 const EXECUTOR_URI = '/api/executors';
 
@@ -17,4 +18,13 @@ export const fetchExecutor = (executorId: string) => (dispatch: Dispatch) => {
 
 export const fetchExecutorRelatedIds = (executorId: string) => {
   return simpleCall(`${EXECUTOR_URI}/${executorId}/related-ids`);
+};
+
+export const fetchConfigurationValueForExecutorType = (executorType: string, configurationKey: string) => {
+  const uri = `${EXECUTOR_URI}?include_next=true`;
+  return simpleCall(uri)
+    .then((response) => fetchExecutorRelatedIds(response.data.find(executor => executor.executor_type === executorType).executor_id))
+    .then((response) => fetchConnectorInstanceConfigurations(response.data.connector_instance_id))
+    .then((response) =>  response.data.find((configuration) => configuration.connector_instance_configuration_key === configurationKey).connector_instance_configuration_value,
+    );
 };
