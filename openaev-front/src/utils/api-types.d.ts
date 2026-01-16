6aaefdc049be3795a6e46b73690157865e352eaa
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-01-16 15:36:08 +0100

    api types
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-front/src/utils/api-types.d.ts
+++ openaev-front/src/utils/api-types.d.ts
@@ -5400,7 +5400,7 @@ export interface Scenario {
   scenario_tags?: string[];
   scenario_teams?: string[];
   scenario_teams_users?: ScenarioTeamUser[];
-  scenario_type_affinity?: "ENDPOINT" | "UNKNOWN";
+  scenario_type_affinity?: string;
   /** @format date-time */
   scenario_updated_at: string;
   scenario_users?: string[];
@@ -5493,7 +5493,7 @@ export interface ScenarioOutput {
   /** @uniqueItems true */
   scenario_teams_users?: ScenarioTeamUserOutput[];
   /** Type affinity of the scenario */
-  scenario_type_affinity?: "ENDPOINT" | "UNKNOWN";
+  scenario_type_affinity?: string;
   /**
    * Update date of the scenario
    * @format date-time
