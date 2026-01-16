ca2fd7c4e0e035fb856bc518969c6397fbca4675
savacano28 <stephanya.casanova@filigran.io>
2026-01-16 19:16:47 +0100

    [backend] fix(securityplatform): feedback review

--- openaev-api/src/main/java/io/openaev/migration/V4_56__Add_unique_constraint_name_security_platform.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_56__Add_unique_constraint_name_security_platform.java
@@ -33,7 +33,7 @@ public class V4_56__Add_unique_constraint_name_security_platform extends BaseJav
                     AND ranked.rn > 1;
               """);
 
-      // 5. Update inject_expectation_results JSON
+      // 2. Update inject_expectation_results JSON
       stmt.execute(
           """
                  UPDATE injects_expectations ie
