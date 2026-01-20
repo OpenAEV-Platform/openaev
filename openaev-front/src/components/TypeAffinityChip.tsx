d27edaac85e624ef14e504af8f03e3f343ec9d34
savacano28 <stephanya.casanova@filigran.io>
2026-01-20 18:17:28 +0100

    [backend] fix: early checking when scenario is undefined

--- openaev-front/src/components/TypeAffinityChip.tsx
+++ openaev-front/src/components/TypeAffinityChip.tsx
@@ -1,29 +1,26 @@
 import { Chip } from '@mui/material';
 import type { FunctionComponent } from 'react';
 
-interface Props { affinity_text: string }
+interface Props {
+  affinity_text?: string | null;
+}
 
 const TypeAffinityChip: FunctionComponent<Props> = ({ affinity_text }) => {
+  if (!affinity_text) return <>-</>;
+
   return (
-    <>
-      { affinity_text
-        && (
-          <Chip
-            variant="outlined"
-            label={affinity_text}
-            style={{
-              fontSize: 12,
-              height: 25,
-              margin: '0 7px 7px 0',
-              textTransform: 'uppercase',
-              borderRadius: 4,
-              width: 180,
-            }}
-          />
-        )}
-      { !affinity_text
-        && '-' }
-    </>
+    <Chip
+      variant="outlined"
+      label={affinity_text.toUpperCase()}
+      style={{
+        fontSize: 12,
+        height: 25,
+        margin: '0 7px 7px 0',
+        textTransform: 'uppercase',
+        borderRadius: 4,
+        width: 180,
+      }}
+    />
   );
 };
 
