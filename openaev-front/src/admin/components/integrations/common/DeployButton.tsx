8317898fa51fd93c84f6df7890fa586996be07b8
Camille Roux <camille.roux@filigran.io>
2026-01-26 14:38:32 +0100

    [backend/frontend] fix(catalog) delete dummy when deploying it connector and update stream

--- openaev-front/src/admin/components/integrations/common/DeployButton.tsx
+++ openaev-front/src/admin/components/integrations/common/DeployButton.tsx
@@ -1,5 +1,4 @@
 import { Badge, Button, Tooltip } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
 import { type CSSProperties, type SyntheticEvent } from 'react';
 
 import { useFormatter } from '../../../../components/i18n';
@@ -14,7 +13,6 @@ interface Props {
 
 const DeployButton = ({ onDeployBtnClick, style = {}, deploymentCount }: Props) => {
   const { t } = useFormatter();
-  const theme = useTheme();
   const {
     isValidated: isEnterpriseEdition,
     openDialog: openEnterpriseEditionDialog,
@@ -34,7 +32,6 @@ const DeployButton = ({ onDeployBtnClick, style = {}, deploymentCount }: Props) 
       <div style={{
         ...style,
         position: 'relative',
-        marginRight: theme.spacing(2),
       }}
       >
         <Button
