3186cef06e4b59b0bbdc46594d7cbc12ab3644a8
savacano28 <stephanya.casanova@filigran.io>
2026-02-04 11:00:08 +0100

    [backend] chore: clean

--- openaev-model/src/main/java/io/openaev/database/model/StepActionClass.java
+++ openaev-model/src/main/java/io/openaev/database/model/StepActionClass.java
@@ -1,6 +1,8 @@
 package io.openaev.database.model;
 
 public enum StepActionClass {
+  //  TODO: Replace String with Class<?> once InjectExecutionStep is implemented
+
   INJECT_EXECUTION("InjectExecutionStep.class");
 
   public final String className;
