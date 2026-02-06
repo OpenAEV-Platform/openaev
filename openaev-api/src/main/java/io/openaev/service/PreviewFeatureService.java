d8a3ce36091ac4f11d04dcfb34d58afff038018a
RomualdLemesle <romuald.lemesle@filigran.io>
2026-02-06 11:23:09 +0100

    [backend/frontend] feature(multi-tenancy): Introduce feature flag

--- openaev-api/src/main/java/io/openaev/service/PreviewFeatureService.java
+++ openaev-api/src/main/java/io/openaev/service/PreviewFeatureService.java
@@ -4,6 +4,7 @@ import static io.openaev.rest.settings.PreviewFeature.FEATURE_FLAG_ALL;
 
 import io.openaev.rest.settings.PreviewFeature;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,8 @@ public class PreviewFeatureService {
   @Cacheable("global")
   public boolean isFeatureEnabled(PreviewFeature feature) {
     List<PreviewFeature> enabledFeatures =
-        platformSettingsService.findSettings().getEnabledDevFeatures();
+        Optional.ofNullable(platformSettingsService.findSettings().getEnabledDevFeatures())
+            .orElse(List.of());
     return enabledFeatures.contains(FEATURE_FLAG_ALL) || enabledFeatures.contains(feature);
   }
 }
