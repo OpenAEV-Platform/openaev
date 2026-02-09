ead4fa250f05aadc68f7fd196a99f6842fca9e05
RomualdLemesle <romuald.lemesle@filigran.io>
2026-02-09 11:25:52 +0100

    [backend] feat(user-event): add user event logic (#4834)

--- openaev-api/src/main/java/io/openaev/migration/V4_69__Add_user_event.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_69__Add_user_event.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_68__Add_user_event extends BaseJavaMigration {
+public class V4_69__Add_user_event extends BaseJavaMigration {
   @Override
   public void migrate(Context context) throws Exception {
     try (Statement stmt = context.getConnection().createStatement()) {
