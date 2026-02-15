1c660a4acba515ca487e58011a72430dd3515221
Tiberiu Sabău <T.Sabau@student.tudelft.nl>
2026-02-15 16:24:10 +0100

    [frontend] fix: Make time more readable in th inject overview
    Signed-off-by: Tiberiu Sabău <T.Sabau@student.tudelft.nl>

--- openaev-front/src/admin/components/atomic_testings/atomic_testing/target_result/TargetResultsReactFlow.tsx
+++ openaev-front/src/admin/components/atomic_testings/atomic_testing/target_result/TargetResultsReactFlow.tsx
@@ -176,7 +176,7 @@ const TargetResultsReactFlow = ({ className = '', injectStatusName, targetResult
       labelShowBg: false,
       labelStyle: {
         fill: theme.palette.text?.primary,
-        fontSize: 9,
+        fontSize: 12,
       },
     };
   };
