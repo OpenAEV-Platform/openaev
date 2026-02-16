2a175343964c50788ad8260438c2a99d3e6757dc
savacano28 <stephanya.casanova@filigran.io>
2026-02-16 16:12:56 +0100

    [backend] feat: wip

--- openaev-model/src/main/java/io/openaev/database/model/ContractOutputType.java
+++ openaev-model/src/main/java/io/openaev/database/model/ContractOutputType.java
@@ -1,44 +1,23 @@
 package io.openaev.database.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.Hidden;
+import lombok.Getter;
 
+@Getter
 public enum ContractOutputType {
-  @JsonProperty("text")
-  Text("text"),
-
-  @JsonProperty("number")
-  Number("number"),
-
-  @JsonProperty("port")
-  Port("port"),
-
-  @JsonProperty("portscan")
-  PortsScan("portscan"),
-
-  @JsonProperty("ipv4")
-  IPv4("ipv4"),
-
-  @JsonProperty("ipv6")
-  IPv6("ipv6"),
-
-  @JsonProperty("credentials")
-  Credentials("credentials"),
-
-  @JsonProperty("cve")
-  CVE("cve"),
-
-  @Hidden
-  @JsonProperty("asset")
-  Asset("asset");
+  @JsonProperty("text") Text("text"),
+  @JsonProperty("number") Number("number"),
+  @JsonProperty("port") Port("port"),
+  @JsonProperty("portscan") PortScan("portscan"),
+  @JsonProperty("ipv4") IPv4("ipv4"),
+  @JsonProperty("ipv6") IPv6("ipv6"),
+  @JsonProperty("credentials") Credentials("credentials"),
+  @JsonProperty("cve") CVE("cve"),
+  @JsonProperty("asset") Asset("asset");
 
   private final String label;
 
   ContractOutputType(String label) {
     this.label = label;
-  }
-
-  public String getLabel() {
-    return label;
   }
 }
