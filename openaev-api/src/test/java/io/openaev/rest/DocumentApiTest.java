f34de84c03b8c9d83518b419f9dee56b7645e2dc
Gabriel PEZÉ <gabriel.peze@filigran.io>
2026-02-18 12:07:14 +0100

    [backend] feat(stix): wip (#3511)

--- openaev-api/src/test/java/io/openaev/rest/DocumentApiTest.java
+++ openaev-api/src/test/java/io/openaev/rest/DocumentApiTest.java
@@ -5,33 +5,33 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import io.openaev.IntegrationTest;
-import io.openaev.database.model.Challenge;
-import io.openaev.database.model.Document;
-import io.openaev.database.model.Domain;
+import io.openaev.database.model.*;
 import io.openaev.database.repository.ChallengeRepository;
 import io.openaev.database.repository.DocumentRepository;
 import io.openaev.rest.document.DocumentService;
+import io.openaev.rest.document.form.DocumentCreateInput;
 import io.openaev.rest.document.form.DocumentRelationsOutput;
 import io.openaev.rest.document.form.RelatedEntityOutput;
 import io.openaev.utils.fixtures.*;
-import io.openaev.utils.fixtures.composers.ChallengeComposer;
-import io.openaev.utils.fixtures.composers.DocumentComposer;
-import io.openaev.utils.fixtures.composers.DomainComposer;
-import io.openaev.utils.fixtures.composers.PayloadComposer;
+import io.openaev.utils.fixtures.composers.*;
 import io.openaev.utils.fixtures.files.BinaryFile;
 import io.openaev.utils.mockUser.WithMockUser;
 import jakarta.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.mock.web.MockPart;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,11 +45,14 @@ class DocumentApiTest extends IntegrationTest {
   @Autowired ChallengeComposer challengeComposer;
   @Autowired PayloadComposer payloadComposer;
   @Autowired DomainComposer domainComposer;
+    @Autowired ScenarioComposer scenarioComposer;
+    @Autowired ExerciseComposer exerciseComposer;
   @Autowired private MockMvc mvc;
   @Autowired private DocumentRepository documentRepository;
   @Autowired private ChallengeRepository challengeRepository;
 
-  @BeforeAll
+
+    @BeforeAll
   void beforeAll() {
     challengeComposer.reset();
     documentComposer.reset();
@@ -142,6 +145,98 @@ class DocumentApiTest extends IntegrationTest {
 
       assertThatJson(response).when(IGNORING_ARRAY_ORDER).isEqualTo(relationJson);
     }
+
+      @Test
+      @DisplayName("Should create a document when uploading a valid file and input")
+      void uploadDocumentShouldCreateDocument() throws Exception {
+          // -- PREPARE
+          Scenario scenario = scenarioComposer.forScenario(ScenarioFixture.getScenario()).persist().get();
+          Exercise exercise = exerciseComposer.forExercise(ExerciseFixture.getExercise()).persist().get();
+
+          DocumentCreateInput input = new DocumentCreateInput();
+          input.setDescription("My test document");
+          input.setScenarioIds(List.of(scenario.getId()));
+          input.setExerciseIds(List.of(exercise.getId()));
+
+          MockPart inputPart = new MockPart(
+                  "input",
+                  mapper.writeValueAsBytes(input)
+          );
+          inputPart.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+          MockMultipartFile filePart = new MockMultipartFile(
+                  "file",
+                  FileFixture.getPngSmileFileContent().getFileName(),
+                  MediaType.APPLICATION_XML.toString(),
+                  FileFixture.getPngSmileFileContent().getContentBytes()
+          );
+
+          // -- EXECUTE
+          String response = mvc.perform(
+                          multipart(DOCUMENT_API + "/upsert")
+                                  .part(inputPart)
+                                  .file(filePart)
+                                  .accept(MediaType.APPLICATION_JSON)
+                  )
+                  .andExpect(status().isOk())
+                  .andReturn()
+                  .getResponse()
+                  .getContentAsString();
+
+          // -- VERIFY
+          assertNotNull(response);
+          assertEquals(FileFixture.getPngSmileFileContent().getFileName(), JsonPath.read(response, "$.document_name"));
+          assertEquals("My test document", JsonPath.read(response, "$.document_description"));
+          assertEquals(scenario.getId(), JsonPath.read(response, "$.document_scenarios[0]"));
+          assertEquals(exercise.getId(), JsonPath.read(response, "$.document_exercises[0]"));
+      }
+
+      @Test
+      @DisplayName("Should update a document when uploading a valid file and input")
+      void uploadDocumentShouldUpdateDocument() throws Exception {
+          // -- PREPARE
+          Scenario scenario = scenarioComposer.forScenario(ScenarioFixture.getScenario()).persist().get();
+          Exercise exercise = exerciseComposer.forExercise(ExerciseFixture.getExercise()).persist().get();
+
+          Document document = documentComposer.forDocument(DocumentFixture.getDocument(FileFixture.getPlainTextFileContent())).persist().get();
+
+          DocumentCreateInput input = new DocumentCreateInput();
+          input.setDescription(document.getDescription());
+          input.setScenarioIds(List.of(scenario.getId()));
+          input.setExerciseIds(List.of(exercise.getId()));
+
+          MockPart inputPart = new MockPart(
+                  "input",
+                  mapper.writeValueAsBytes(input)
+          );
+          inputPart.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+          MockMultipartFile filePart = new MockMultipartFile(
+                  "file",
+                  FileFixture.getPlainTextFileContent().getFileName(),
+                  MediaType.APPLICATION_XML.toString(),
+                  FileFixture.getPlainTextFileContent().getContentBytes()
+          );
+
+          // -- EXECUTE
+          String response = mvc.perform(
+                          multipart(DOCUMENT_API + "/upsert")
+                                  .part(inputPart)
+                                  .file(filePart)
+                                  .accept(MediaType.APPLICATION_JSON)
+                  )
+                  .andExpect(status().isOk())
+                  .andReturn()
+                  .getResponse()
+                  .getContentAsString();
+
+          // -- VERIFY
+          assertNotNull(response);
+          assertEquals(FileFixture.getPngSmileFileContent().getFileName(), JsonPath.read(response, "$.document_name"));
+          assertEquals("My test document", JsonPath.read(response, "$.document_description"));
+          assertEquals(scenario.getId(), JsonPath.read(response, "$.document_scenarios[0]"));
+          assertEquals(exercise.getId(), JsonPath.read(response, "$.document_exercises[0]"));
+      }
   }
 
   @Test
