021a9297111751ffac029a93a1cdd081abf4948d
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2026-02-25 17:25:09 +0100

    fix test

--- openaev-model/src/main/java/io/openaev/database/model/Scenario.java
+++ openaev-model/src/main/java/io/openaev/database/model/Scenario.java
@@ -258,10 +258,7 @@ public class Scenario implements GrantableBase {
   private List<Grant> grants = new ArrayList<>();
 
   @Schema(implementation = String[].class)
-  @OneToMany(
-      mappedBy = "scenario",
-      cascade = CascadeType.ALL,
-      fetch = FetchType.LAZY)
+  @OneToMany(mappedBy = "scenario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   @JsonProperty("scenario_injects")
   @JsonSerialize(using = MultiIdListSerializer.class)
   @Getter(NONE)
