d5e5e1a635d2ba9bc84ae689d918066bb88f0e5f
Gabriel Pezé <gabriel.peze@filigran.io>
2026-03-02 09:10:38 +0100

    [backend] feat(stix): artifact management (#3511)

--- openaev-api/src/main/java/io/openaev/service/stix/SecurityCoverageService.java
+++ openaev-api/src/main/java/io/openaev/service/stix/SecurityCoverageService.java
@@ -3,7 +3,6 @@ package io.openaev.service.stix;
 import static io.openaev.helper.CryptoHelper.md5Hex;
 import static io.openaev.rest.payload.service.PayloadService.DYNAMIC_DNS_RESOLUTION_HOSTNAME_KEY;
 import static io.openaev.stix.objects.constants.CommonProperties.MODIFIED;
-import static io.openaev.utils.SecurityCoverageUtils.extractObjectReferences;
 import static io.openaev.utils.constants.StixConstants.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -40,6 +39,7 @@ import io.openaev.stix.types.Boolean;
 import io.openaev.stix.types.Dictionary;
 import io.openaev.utils.InjectExpectationResultUtils;
 import io.openaev.utils.ResultUtils;
+import io.openaev.utils.SecurityCoverageUtils;
 import io.openaev.utils.StringUtils;
 import io.openaev.utils.time.TimeUtils;
 import jakarta.annotation.Resource;
@@ -84,6 +84,8 @@ public class SecurityCoverageService {
   // FIXME: don't access the connector directly when we deal with multiple origins
   private final SecurityCoverageConnector connector;
 
+  private final SecurityCoverageUtils securityCoverageUtils;
+
   /**
    * Creates or updates a security coverage and the associated scenario, and updates the
    * corresponding connector.
@@ -103,10 +105,11 @@ public class SecurityCoverageService {
       throws ParsingException, BundleValidationError, ConnectorError, IOException {
     String bundleHash = md5Hex(bundle.toStix(objectMapper).toString());
 
-    SecurityCoverage securityCoverage =
-        buildSecurityCoverageFromStix(
-            securityCoverageObj, bundle, securityCoverageStixId, bundleHash);
-    Scenario scenario = buildScenarioFromSecurityCoverage(securityCoverage);
+    JsonNode root = objectMapper.readTree(stixJson);
+    String stixJsonHash = md5Hex(stixJson);
+    Bundle bundle = stixParser.parseBundle(root.toString());
+    ObjectBase stixCoverageObj = securityCoverageUtils.extractAndValidateCoverage(bundle);
+    String externalId = stixCoverageObj.getRequiredProperty(CommonProperties.ID.toString());
 
     // FIXME: extract this behaviour into an async worker
     pushSecurityCoverageBundleWithExternalURI(scenario);
@@ -179,15 +182,21 @@ public class SecurityCoverageService {
 
     // Extract Attack Patterns
     securityCoverage.setAttackPatternRefs(
-        extractObjectReferences(bundle.findByType(ObjectTypes.ATTACK_PATTERN)));
+        securityCoverageUtils.extractObjectReferences(
+            bundle.findByType(ObjectTypes.ATTACK_PATTERN)));
 
     // Extract vulnerabilities
     securityCoverage.setVulnerabilitiesRefs(
-        extractObjectReferences(bundle.findByType(ObjectTypes.VULNERABILITY)));
+        securityCoverageUtils.extractObjectReferences(
+            bundle.findByType(ObjectTypes.VULNERABILITY)));
 
     // Extract indicators
     securityCoverage.setIndicatorsRefs(
-        extractObjectReferences(bundle.findByType(ObjectTypes.INDICATOR)));
+        securityCoverageUtils.extractObjectReferences(bundle.findByType(ObjectTypes.INDICATOR)));
+
+    // Extract artifacts
+    securityCoverage.setArtifactsRefs(
+        securityCoverageUtils.extractObjectReferences(bundle.findByType(ObjectTypes.ARTIFACT)));
 
     // Default Fields
     String scheduling = stixCoverageObj.getOptionalProperty(STIX_PERIODICITY, "");
@@ -484,8 +493,19 @@ public class SecurityCoverageService {
           objects);
     }
 
-    for (SecurityPlatform securityPlatform :
-        injectService.extractSecurityPlatforms(simulation.getInjects())) {
+    if (simulation.getSecurityCoverage().getArtifactsRefs() != null
+        && !simulation.getSecurityCoverage().getArtifactsRefs().isEmpty()) {
+      processCoverageRefs(
+          simulation.getSecurityCoverage().getArtifactsRefs(),
+          simulation,
+          this::getArtifactCoverage,
+          coverage.getId(),
+          sroStartTime,
+          sroStopTime,
+          objects);
+    }
+
+    for (SecurityPlatform securityPlatform : assetService.securityPlatforms()) {
       DomainObject platformIdentity = securityPlatform.toStixDomainObject();
       objects.add(platformIdentity);
 
@@ -524,13 +544,13 @@ public class SecurityCoverageService {
   private void processCoverageRefs(
       Set<StixRefToExternalRef> refs,
       Exercise simulation,
-      BiFunction<String, Exercise, BaseType<?>> coverageFunction,
+      BiFunction<List<String>, Exercise, BaseType<?>> coverageFunction,
       Identifier coverageId,
       Optional<Timestamp> sroStartTime,
       Optional<Timestamp> sroStopTime,
       List<ObjectBase> objects) {
     for (StixRefToExternalRef stixRef : refs) {
-      BaseType<?> coverageResult = coverageFunction.apply(stixRef.getExternalRef(), simulation);
+      BaseType<?> coverageResult = coverageFunction.apply(stixRef.getExternalRefs(), simulation);
       boolean covered = !((List<?>) coverageResult.getValue()).isEmpty();
 
       RelationshipObject sro =
@@ -571,11 +591,11 @@ public class SecurityCoverageService {
     return computeCoverageFromInjects(simulation.getInjects(), securityPlatform);
   }
 
-  private BaseType<?> getVulnerabilityCoverage(String externalRef, Exercise simulation) {
+  private BaseType<?> getVulnerabilityCoverage(List<String> externalRefs, Exercise simulation) {
     return getCoverage(
-        externalRef,
+        externalRefs,
         simulation,
-        id -> vulnerabilityService.getVulnerabilitiesByExternalIds(Set.of(id)),
+        ids -> vulnerabilityService.getVulnerabilitiesByExternalIds(new HashSet<>(ids)),
         inject -> {
           if (inject.getInjectorContract().isPresent()) {
             return inject.getInjectorContract().get().getVulnerabilities();
@@ -585,11 +605,11 @@ public class SecurityCoverageService {
         Vulnerability::getId);
   }
 
-  private BaseType<?> getAttackPatternCoverage(String externalRef, Exercise simulation) {
+  private BaseType<?> getAttackPatternCoverage(List<String> externalRefs, Exercise simulation) {
     return getCoverage(
-        externalRef,
+        externalRefs,
         simulation,
-        id -> attackPatternService.getAttackPatternsByExternalIds(Set.of(id)),
+        ids -> attackPatternService.getAttackPatternsByExternalIds(new HashSet<>(ids)),
         inject -> {
           if (inject.getInjectorContract().isPresent()) {
             return inject.getInjectorContract().get().getAttackPatterns();
@@ -599,21 +619,40 @@ public class SecurityCoverageService {
         AttackPattern::getId);
   }
 
-  private BaseType<?> getDnsIndicatorCoverage(String externalRef, Exercise simulation) {
+  private BaseType<?> getDnsIndicatorCoverage(List<String> externalRefs, Exercise simulation) {
     return getCoverage(
-        externalRef,
+        externalRefs,
         simulation,
-        hostname ->
+        hostnames ->
             simulation.getInjects().stream()
                 .filter(
                     inject ->
                         inject.getContent().has(DYNAMIC_DNS_RESOLUTION_HOSTNAME_KEY)
-                            && hostname.equals(
-                                inject
+                            && hostnames.contains(
+                                (inject
                                     .getContent()
                                     .get(DYNAMIC_DNS_RESOLUTION_HOSTNAME_KEY)
-                                    .textValue()))
-                .collect(Collectors.toList()),
+                                    .textValue())))
+                .toList(),
+        inject ->
+            Optional.ofNullable(inject)
+                .map(Collections::singletonList)
+                .orElse(Collections.emptyList()),
+        Inject::getId);
+  }
+
+  private BaseType<?> getArtifactCoverage(List<String> externalRefs, Exercise simulation) {
+    return getCoverage(
+        externalRefs,
+        simulation,
+        documentIds ->
+            simulation.getInjects().stream()
+                .filter(
+                    inject ->
+                        inject.getPayload().isPresent()
+                            && inject.getPayload().get() instanceof FileDrop fileDrop
+                            && documentIds.contains(fileDrop.getFileDropFile().getId()))
+                .toList(),
         inject ->
             Optional.ofNullable(inject)
                 .map(Collections::singletonList)
@@ -622,13 +661,13 @@ public class SecurityCoverageService {
   }
 
   private <T> BaseType<?> getCoverage(
-      String externalRef,
+      List<String> externalRefs,
       Exercise simulation,
-      Function<String, Collection<T>> entityFetcher,
+      Function<List<String>, Collection<T>> entityFetcher,
       Function<Inject, Collection<T>> contractExtractor,
       Function<T, String> idExtractor) {
     // fetch entity
-    Optional<T> entity = entityFetcher.apply(externalRef).stream().findFirst();
+    Optional<T> entity = entityFetcher.apply(externalRefs).stream().findFirst();
     if (entity.isEmpty()) {
       return uncovered();
     }
