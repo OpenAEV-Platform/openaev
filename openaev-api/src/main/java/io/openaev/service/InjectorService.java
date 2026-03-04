a76c9127414edc6310713cc70ae422ed87028a7d
damgouj <damien.goujard@filigran.io>
2026-03-04 15:34:20 +0100

    [backend] feat(multi-tenancy): manage attack_patterns table (#3505)

--- openaev-api/src/main/java/io/openaev/service/InjectorService.java
+++ openaev-api/src/main/java/io/openaev/service/InjectorService.java
@@ -319,8 +319,9 @@ public class InjectorService extends AbstractConnectorService<Injector, Injector
                 if (!current.get().getAttackPatternsExternalIds().isEmpty()) {
                   List<AttackPattern> attackPatterns =
                       fromIterable(
-                          attackPatternRepository.findAllByExternalIdInIgnoreCase(
-                              current.get().getAttackPatternsExternalIds()));
+                          attackPatternRepository.findAllByExternalIdInIgnoreCaseAndTenantId(
+                              current.get().getAttackPatternsExternalIds(),
+                              injector.getTenant().getId()));
                   contract.setAttackPatterns(attackPatterns);
                 } else {
                   contract.setAttackPatterns(new ArrayList<>());
