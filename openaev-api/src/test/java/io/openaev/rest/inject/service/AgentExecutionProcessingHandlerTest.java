5deb634a570be0b02b29d3f62b0f665b32f659db
savacano28 <stephanya.casanova@filigran.io>
2026-03-05 14:01:52 +0100

    [backend] feat: review feedback

--- openaev-api/src/test/java/io/openaev/rest/inject/service/AgentExecutionProcessingHandlerTest.java
+++ openaev-api/src/test/java/io/openaev/rest/inject/service/AgentExecutionProcessingHandlerTest.java
@@ -1,6 +1,7 @@
 package io.openaev.rest.inject.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
