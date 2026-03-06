cda9f7077afb1bd8f73698bfeeec181e19f96ec3
Damien Goujard <damien.goujard@filigran.io>
2026-03-06 15:52:15 +0100

    [backend] Fix

--- openaev-model/src/main/java/io/openaev/database/repository/DocumentRepository.java
+++ openaev-model/src/main/java/io/openaev/database/repository/DocumentRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
