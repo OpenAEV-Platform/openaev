ab9b7fa352059289eae5dde805ba91136ff81812
Gael Leblan <gael.leblan@filigran.io>
2026-03-10 17:50:03 +0100

    [backend] feat(injects): target a specific injector instance for execution (#4862)

--- openaev-model/src/main/java/io/openaev/database/model/Inject.java
+++ openaev-model/src/main/java/io/openaev/database/model/Inject.java
@@ -178,6 +178,7 @@ public class Inject implements GrantableBase, Injection, TenantBase {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "inject_injector")
   @JsonSerialize(using = MonoIdSerializer.class)
+  @JsonDeserialize(using = MonoIdDeserializerHelper.class)
   @JsonProperty("inject_injector")
   @Schema(type = "string")
   private Injector injector;
