a66aacfee23593a7839cb40e933520c35c411656
Gael Leblan <gael.leblan@filigran.io>
2026-03-16 23:24:55 +0100

    [backend] feat(injects): target a specific injector instance for execution (#4862)

--- openaev-front/src/utils/api-types.d.ts
+++ openaev-front/src/utils/api-types.d.ts
@@ -3396,6 +3396,7 @@ export interface Inject {
   inject_expectations?: string[];
   /** @minLength 1 */
   inject_id: string;
+  inject_injector?: string;
   inject_injector_contract?: InjectorContract;
   inject_kill_chain_phases?: KillChainPhase[];
   inject_scenario?: string;
@@ -3738,6 +3739,7 @@ export interface InjectInput {
   inject_description?: string;
   inject_documents?: InjectDocumentInput[];
   inject_enabled?: boolean;
+  inject_injector?: string;
   inject_injector_contract?: string;
   inject_tags?: string[];
   inject_teams?: string[];
