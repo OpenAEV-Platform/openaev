dbd7741efb57a706eee235e04f0fc22c01c7f0d3
Damien Goujard <damien.goujard@filigran.io>
2026-03-17 15:20:41 +0100

    [backend] Fix

--- openaev-model/src/main/java/io/openaev/multitenancy/DependenciesManager.java
+++ openaev-model/src/main/java/io/openaev/multitenancy/DependenciesManager.java
@@ -1,8 +1,10 @@
 package io.openaev.multitenancy;
 
 import io.openaev.database.model.Tenant;
+import org.springframework.context.annotation.Profile;
 
 /** Interface to create and delete all the necessary elements at tenant creation/deletion */
+@Profile("!test")
 public interface DependenciesManager {
 
   void createDependencyForTenant(Tenant tenant) throws DependenciesManagerException;
