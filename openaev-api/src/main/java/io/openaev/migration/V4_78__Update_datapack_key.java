3767fcb89189c599d19ce5dd427d5cc16d2e42c5
Damien Goujard <damien.goujard@filigran.io>
2026-03-24 09:19:37 +0100

    [backend] Fix unique name tag

--- openaev-api/src/main/java/io/openaev/migration/V4_78__Update_datapack_key.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_78__Update_datapack_key.java
@@ -21,6 +21,19 @@ public class V4_78__Update_datapack_key extends BaseJavaMigration {
                         ALTER TABLE datapacks
                         ADD CONSTRAINT datapacks_pkey UNIQUE (datapack_id, tenant_id);
                         """);
+      statement.execute(
+          """
+                        DELETE FROM parameters WHERE parameter_key = 'starterpack';
+                        """);
+      statement.execute(
+          """
+                        DROP INDEX IF EXISTS tag_name_unique;
+                        """);
+      statement.execute(
+          """
+                        ALTER TABLE tags
+                        ADD CONSTRAINT tag_name_tenant_unique UNIQUE (tag_name, tenant_id);
+                        """);
     }
   }
 }
