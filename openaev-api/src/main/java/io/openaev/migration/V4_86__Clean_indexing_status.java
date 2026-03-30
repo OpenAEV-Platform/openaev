904ca0a36217d2c76228cca8a347c4331af3120d
Gael Leblan <gael.leblan@filigran.io>
2026-03-30 16:48:03 +0200

    [backend] feat(multitenancy): rename migration (#3505)

--- openaev-api/src/main/java/io/openaev/migration/V4_86__Clean_indexing_status.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_86__Clean_indexing_status.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-public class V4_79__Clean_indexing_status extends BaseJavaMigration {
+public class V4_86__Clean_indexing_status extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
