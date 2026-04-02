3712d3a27241efdfbe136785b24af40ad396b346
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2026-04-02 23:08:50 +0200

    [backend] feature(chaining): adding RBAC resource types for chaining (#4824)

--- openaev-model/src/main/java/io/openaev/database/model/ResourceType.java
+++ openaev-model/src/main/java/io/openaev/database/model/ResourceType.java
@@ -49,6 +49,8 @@ public enum ResourceType {
   // Special resource types
   UNKNOWN,
   SIMULATION_OR_SCENARIO, // Used to represent either a simulation or a scenario.
+  WORKFLOW,
+  STEP,
   SKIP_RBAC; // Used to skip RBAC checks.
 
   public static ResourceType fromString(@NotNull String name) {
