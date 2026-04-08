b942670fc0ac282acff6cb06457ae53972bb27ac
Camille Roux <camille.roux@filigran.io>
2026-04-08 17:25:34 +0200

    [backend] feature(chaining): Step CRUD

--- openaev-api/src/main/java/io/openaev/migration/V4_90__Update_conditions_for_condition_tree.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_90__Update_conditions_for_condition_tree.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_88__Update_conditions_for_condition_tree extends BaseJavaMigration {
+public class V4_90__Update_conditions_for_condition_tree extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
