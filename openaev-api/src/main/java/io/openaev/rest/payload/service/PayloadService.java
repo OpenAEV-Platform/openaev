ed40126d65e158ba7a3a9ee8721a67530c09a1b4
Marine LM <marine.lemezo@filigran.io>
2026-04-09 10:53:43 +0200

    [backend] fix: create ThreatArsenal Api
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-api/src/main/java/io/openaev/rest/payload/service/PayloadService.java
+++ openaev-api/src/main/java/io/openaev/rest/payload/service/PayloadService.java
@@ -42,7 +42,6 @@ import io.openaev.rest.domain.DomainService;
 import io.openaev.rest.domain.enums.PresetDomain;
 import io.openaev.rest.exception.ElementNotFoundException;
 import io.openaev.rest.injector_contract.form.InjectorContractDomainDTO;
-import io.openaev.rest.payload.PayloadApi;
 import io.openaev.rest.payload.PayloadUtils;
 import io.openaev.rest.payload.output.PayloadOutput;
 import io.openaev.rest.tag.TagService;
