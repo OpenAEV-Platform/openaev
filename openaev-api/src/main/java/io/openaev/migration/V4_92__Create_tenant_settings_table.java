e4d35d1d8aaed6010d98914718e64c8baaae0193
Corinne Krych <corinne.krych@filigran.io>
2026-04-14 10:28:10 +0200

    [backend] fix(multi-tenancy): rename flyway migration number

--- openaev-api/src/main/java/io/openaev/migration/V4_92__Create_tenant_settings_table.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_92__Create_tenant_settings_table.java
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_90__Create_tenant_settings_table extends BaseJavaMigration {
+public class V4_92__Create_tenant_settings_table extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
