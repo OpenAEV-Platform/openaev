62a27e5ce994843e6f06aaceb7b0183d11fb6765
savacano28 <stephanya.casanova@filigran.io>
2026-04-16 21:23:15 +0200

    [backend] feat(chaining): rebase

--- openaev-api/src/main/java/io/openaev/migration/V4_95__Add_mapping_type_to_conditions.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_95__Add_mapping_type_to_conditions.java
@@ -26,6 +26,11 @@ public class V4_95__Add_mapping_type_to_conditions extends BaseJavaMigration {
           "ALTER TABLE conditions ADD COLUMN IF NOT EXISTS condition_mapping_type mapping_type;");
       stmt.execute(
           "CREATE INDEX IF NOT EXISTS idx_conditions_mapping_type ON conditions(condition_mapping_type);");
+      stmt.execute(
+          "UPDATE conditions "
+              + "SET condition_key = condition_key_type "
+              + "WHERE condition_key IS NULL "
+              + "AND condition_type = 'MAPPER';");
     }
   }
 }
