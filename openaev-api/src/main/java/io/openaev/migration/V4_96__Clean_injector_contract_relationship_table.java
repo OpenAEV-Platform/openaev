6dd766583672c62ef70172fbc75a4b56f73f6f0b
Marine LM <marine.lemezo@filigran.io>
2026-04-17 09:03:01 +0200

    [backend] feat(threat-arsenal): upgrade migration version
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-api/src/main/java/io/openaev/migration/V4_96__Clean_injector_contract_relationship_table.java
+++ openaev-api/src/main/java/io/openaev/migration/V4_96__Clean_injector_contract_relationship_table.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V4_95__Clean_injector_contract_relationship_table extends BaseJavaMigration {
+public class V4_96__Clean_injector_contract_relationship_table extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
