b5d34c02663b7557e81b76ed6e95a6f9582a8ed6
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-04-20 17:31:29 +0200

    make password reset asynchronous
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-api/src/test/java/io/openaev/rest/UserApiTest.java
+++ openaev-api/src/test/java/io/openaev/rest/UserApiTest.java
@@ -36,8 +36,10 @@ import io.openaev.utils.fixtures.composers.UserComposer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import net.javacrumbs.jsonunit.core.Option;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.*;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -297,7 +299,18 @@ class UserApiTest extends IntegrationTest {
 
       // -- ASSERT --
       ArgumentCaptor<List<User>> userCaptor = ArgumentCaptor.forClass(List.class);
-      verify(mailingService).sendEmail(anyString(), anyString(), userCaptor.capture());
+      // not ideal, but the actual reset happens in a background thread!
+      Awaitility.await()
+          .atMost(1, TimeUnit.SECONDS)
+          .until(
+              () -> {
+                try {
+                  verify(mailingService).sendEmail(anyString(), anyString(), userCaptor.capture());
+                  return true;
+                } catch (Exception e) {
+                  return false;
+                }
+              });
       assertEquals(EMAIL, userCaptor.getValue().get(0).getEmail());
     }
 
@@ -319,11 +332,41 @@ class UserApiTest extends IntegrationTest {
                   .content(asJsonString(input)))
           .andExpect(status().isOk());
 
+      // not ideal, but the actual reset happens in a background thread!
+      Awaitility.await()
+          .atMost(1, TimeUnit.SECONDS)
+          .until(
+              () -> {
+                try {
+                  ArgumentCaptor<List<User>> userCaptor = ArgumentCaptor.forClass(List.class);
+                  verify(mailingService, times(1))
+                      .sendEmail(anyString(), anyString(), userCaptor.capture());
+                  return true;
+                } catch (Exception e) {
+                  return false;
+                }
+              });
+
       mvc.perform(
               post("/api/reset")
                   .contentType(MediaType.APPLICATION_JSON)
                   .content(asJsonString(input)))
           .andExpect(status().isOk());
+
+      // not ideal, but the actual reset happens in a background thread!
+      Awaitility.await()
+          .atMost(1, TimeUnit.SECONDS)
+          .until(
+              () -> {
+                try {
+                  ArgumentCaptor<List<User>> userCaptor = ArgumentCaptor.forClass(List.class);
+                  verify(mailingService, times(2))
+                      .sendEmail(anyString(), anyString(), userCaptor.capture());
+                  return true;
+                } catch (Exception e) {
+                  return false;
+                }
+              });
 
       // -- ASSERT --
       mvc.perform(
