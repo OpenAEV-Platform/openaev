47b1c77ce101892933804c88481abf11c567dec0
Romuald Lemesle <romuald.lemesle@filigran.io>
2026-04-21 17:18:24 +0200

    [frontend] feat(multi-tenancy): ddd tenant identifier on settings (#4864)

--- openaev-front/src/admin/components/settings/PlatformInfoPanel.tsx
+++ openaev-front/src/admin/components/settings/PlatformInfoPanel.tsx
@@ -8,10 +8,11 @@ import type { PlatformSettings } from '../../../utils/api-types';
 
 interface PlatformInfoPanelProps {
   settings: PlatformSettings;
-  children?: ReactNode;
+  header?: ReactNode;
+  footer?: ReactNode;
 }
 
-const PlatformInfoPanel = ({ settings, children }: PlatformInfoPanelProps) => {
+const PlatformInfoPanel = ({ settings, header, footer }: PlatformInfoPanelProps) => {
   const { t } = useFormatter();
   const isEnterpriseEditionValid = settings.platform_license?.license_is_validated;
 
@@ -41,6 +42,7 @@ const PlatformInfoPanel = ({ settings, children }: PlatformInfoPanelProps) => {
       }}
     >
       <List sx={{ padding: 0 }}>
+        {header}
         <ListItem divider>
           <ListItemText primary={t('Platform identifier')} />
           <pre
@@ -61,7 +63,7 @@ const PlatformInfoPanel = ({ settings, children }: PlatformInfoPanelProps) => {
           <ListItemText primary={t('Edition')} />
           <ItemBoolean variant="large" neutralLabel={editionLabel} status={null} />
         </ListItem>
-        <ListItem divider={!!children}>
+        <ListItem divider={!!footer}>
           <ListItemText primary={t('AI Powered')} />
           <ItemBoolean
             variant="large"
@@ -70,7 +72,7 @@ const PlatformInfoPanel = ({ settings, children }: PlatformInfoPanelProps) => {
             tooltip={aiTooltip}
           />
         </ListItem>
-        {children}
+        {footer}
       </List>
     </Paper>
   );
