e6ee4a8a6f2b6380d496a00704bf671f24969f99
Antoine Quintin <antoine.quintin@filigran.io>
2026-04-24 10:22:30 +0200

    [frontend] refactor(import-from-hub): reuse getCurrentTenantId helper (#5529)

--- openaev-front/src/admin/components/common/ImportFromHubButton.tsx
+++ openaev-front/src/admin/components/common/ImportFromHubButton.tsx
@@ -3,7 +3,7 @@ import { useTheme } from '@mui/material/styles';
 
 import { useFormatter } from '../../../components/i18n';
 import useAuth from '../../../utils/hooks/useAuth';
-import { DEFAULT_TENANT_UUID } from '../../../utils/tenant-url-helper';
+import { getCurrentTenantId } from '../../../utils/tenant-url-helper';
 import { getUrl, isNotEmptyField } from '../../../utils/utils';
 import GradientButton from './GradientButton';
 
@@ -12,12 +12,12 @@ interface ImportFromHubButtonProps extends ButtonProps { serviceIdentifier: stri
 const ImportFromHubButton = ({ serviceIdentifier }: ImportFromHubButtonProps) => {
   const { t } = useFormatter();
   const theme = useTheme();
-  const { settings, currentUserTenant } = useAuth();
+  const { settings } = useAuth();
   if (!settings.xtm_hub_enable) {
     return null;
   }
 
-  const tenantId = currentUserTenant?.tenant_id ?? DEFAULT_TENANT_UUID;
+  const tenantId = getCurrentTenantId();
   const importFromHubUrl = isNotEmptyField(settings?.xtm_hub_url)
     ? getUrl(
         `/redirect/${serviceIdentifier}?platform_id=${settings.platform_id}&tenant_id=${tenantId}`,
