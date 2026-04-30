773dd0ee211ee47ab93c808ae5bd2099c0c1d627
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-04-30 18:02:22 +0200

    spotless
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-api/src/main/java/io/openaev/service/stix/StixService.java
+++ openaev-api/src/main/java/io/openaev/service/stix/StixService.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
