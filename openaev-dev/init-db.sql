3beeab2870f334c28554c82e00fc1877924839ad
Corinne Krych <corinne.krych@filigran.io>
2026-05-05 18:29:08 +0200

    [backend] chore(multi-tenancy): fix drone to run api-test in CI

--- openaev-dev/init-db.sql
+++ openaev-dev/init-db.sql
@@ -14,3 +14,6 @@ GRANT openaev_app TO openaev_owner;
 ALTER DATABASE openaev OWNER TO openaev_owner;
 ALTER SCHEMA public OWNER TO openaev_owner;
 
+-- 5. Set default app.current_tenant so RLS policies don't fail on empty setting
+ALTER DATABASE openaev SET app.current_tenant = '2cffad3a-0001-4078-b0e2-ef74274022c3';
+
