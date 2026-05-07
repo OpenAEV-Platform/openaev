190593d4926dd95fd3c200ab72a234ccba63cc21
savacano28 <stephanya.casanova@filigran.io>
2026-05-07 18:23:32 +0200

    [frontend] fix: allow update payloads withh paylaod arguments

--- openaev-front/src/admin/components/threat_arsenal/ThreatArsenalActionForm.tsx
+++ openaev-front/src/admin/components/threat_arsenal/ThreatArsenalActionForm.tsx
@@ -2,7 +2,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Box, Button } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { type SyntheticEvent, useEffect } from 'react';
-import { type FieldValues, FormProvider, type SubmitHandler, useForm, useWatch } from 'react-hook-form';
+import { type FieldValues, FormProvider, type Resolver, type SubmitHandler, useForm, useWatch } from 'react-hook-form';
 import { z, type ZodTypeAny } from 'zod';
 
 import Tabs, { type TabsEntry } from '../../../components/common/tabs/Tabs';
@@ -159,7 +159,7 @@ const ThreatArsenalActionForm = ({
 
   const methods = useForm<ThreatArsenalActionCreateCustomInput>({
     mode: 'onTouched',
-    resolver: zodResolver(schema),
+    resolver: zodResolver(schema) as Resolver<ThreatArsenalActionCreateCustomInput>,
     defaultValues: initialValues,
   });
   const {
@@ -227,7 +227,7 @@ const ThreatArsenalActionForm = ({
       focusFirstErrorTab();
       return;
     }
-    await handleSubmit(onSubmit)(e);
+    await handleSubmit(onSubmit as SubmitHandler<FieldValues>)(e);
   };
 
   const trackedUseWatch = useWatch({
