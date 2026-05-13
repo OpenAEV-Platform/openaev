86b459aa425b4627705a68a03fd2a75012b1948a
johanah29 <johanah.lekeu@filigran.io>
2026-05-13 11:16:52 +0200

    Drone errors
    Signed-off-by: johanah29 <johanah.lekeu@filigran.io>

--- openaev-front/src/admin/components/common/entreprise_edition/EnterpriseEditionAgreementDialog.tsx
+++ openaev-front/src/admin/components/common/entreprise_edition/EnterpriseEditionAgreementDialog.tsx
@@ -10,7 +10,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 
-import { Alert, Button, Link, TextField } from '@mui/material';
+import { Alert, Button, TextField } from '@mui/material';
 import { useState } from 'react';
 import { makeStyles } from 'tss-react/mui';
 
