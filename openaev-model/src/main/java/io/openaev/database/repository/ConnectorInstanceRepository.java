dc048032f01b8ab007d902d8790aba72c7ebb805
johanah29 <johanah.lekeu@filigran.io>
2026-05-19 17:11:36 +0200

    resolve drone errors
    Signed-off-by: johanah29 <johanah.lekeu@filigran.io>

--- openaev-model/src/main/java/io/openaev/database/repository/ConnectorInstanceRepository.java
+++ openaev-model/src/main/java/io/openaev/database/repository/ConnectorInstanceRepository.java
@@ -15,10 +15,11 @@ public interface ConnectorInstanceRepository
         JpaSpecificationExecutor<ConnectorInstancePersisted> {
 
   @Query(
-      value = "SELECT DISTINCT ci.* FROM connector_instances ci "
-          + "JOIN catalog_connectors cc ON ci.connector_instance_catalog_id = cc.catalog_connector_id "
-          + "WHERE cc.catalog_connector_container_image IS NOT NULL "
-          + "AND cc.catalog_connector_manager_supported = TRUE ",
+      value =
+          "SELECT DISTINCT ci.* FROM connector_instances ci "
+              + "JOIN catalog_connectors cc ON ci.connector_instance_catalog_id = cc.catalog_connector_id "
+              + "WHERE cc.catalog_connector_container_image IS NOT NULL "
+              + "AND cc.catalog_connector_manager_supported = TRUE ",
       nativeQuery = true)
   List<ConnectorInstancePersisted> findAllManagedByXtmComposerAndConfiguration();
 
