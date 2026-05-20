abe2a7453d5925f97b4e19ef31c902b62cfcb4cf
Gael Leblan <gael.leblan@filigran.io>
2026-05-20 10:03:25 +0200

    [backend] feature(audit-log): adding feature flag (#5480)

--- openaev-api/src/main/java/io/openaev/service/LogService.java
+++ openaev-api/src/main/java/io/openaev/service/LogService.java
@@ -9,6 +9,7 @@ import io.openaev.database.model.ResourceType;
 import io.openaev.database.model.User;
 import io.openaev.engine.EngineService;
 import io.openaev.engine.model.log.LogEvent;
+import io.openaev.rest.settings.PreviewFeature;
 import io.openaev.utils.HttpReqRespUtils;
 import io.openaev.utils.log.LogUtils;
 import io.openaev.utils.log.dispatcher.AuditLogTransportDispatcherUtils;
@@ -44,6 +45,7 @@ public class LogService {
   }
 
   private final GenericLogTransportDispatcherUtils genericLogTransportDispatcherUtils;
+  private final PreviewFeatureService previewFeatureService;
 
   private final AuditLogTransportDispatcherUtils auditLogTransportDispatcherUtils;
 
@@ -65,13 +67,15 @@ public class LogService {
       GenericLogTransportDispatcherUtils genericLogTransportDispatcherUtils,
       AuditLogTransportDispatcherUtils auditLogTransportDispatcherUtils,
       ObjectNormalizationUtils objectNormalizationUtils,
-      ObjectDiffUtils objectDiffUtils) {
+      ObjectDiffUtils objectDiffUtils,
+      PreviewFeatureService previewFeatureService) {
     this.userService = userService;
     this.objectMapper = engineService.getObjectMapper();
     this.genericLogTransportDispatcherUtils = genericLogTransportDispatcherUtils;
     this.auditLogTransportDispatcherUtils = auditLogTransportDispatcherUtils;
     this.objectNormalizationUtils = objectNormalizationUtils;
     this.objectDiffUtils = objectDiffUtils;
+    this.previewFeatureService = previewFeatureService;
   }
 
   // -- Public API --
@@ -303,7 +307,7 @@ public class LogService {
 
   private boolean isEnabled(AuditLogType logType) {
     return logType == AuditLogType.AUDIT
-        ? auditLogsEnabled
+        ? auditLogsEnabled && previewFeatureService.isFeatureEnabled(PreviewFeature.AUDIT_LOG)
         : genericLogsEnabled || auditLogsEnabled;
   }
 
