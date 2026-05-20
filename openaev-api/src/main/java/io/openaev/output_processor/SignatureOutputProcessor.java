55b81ad686f4bde1bc875089badf4ad68c6ec9a6
Gabriel PEZÉ <gabriel.peze@filigran.io>
2026-05-20 10:17:10 +0200

    [backend] feat(signature): pr feedback (#5446)

--- openaev-api/src/main/java/io/openaev/output_processor/SignatureOutputProcessor.java
+++ openaev-api/src/main/java/io/openaev/output_processor/SignatureOutputProcessor.java
@@ -5,14 +5,14 @@ import io.openaev.database.model.ContractOutputTechnicalType;
 import io.openaev.database.model.ContractOutputType;
 import io.openaev.rest.inject.service.ContractOutputContext;
 import io.openaev.rest.inject.service.ExecutionProcessingContext;
-import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SignatureOutputProcessor extends AbstractOutputProcessor {
 
   protected SignatureOutputProcessor() {
-    super(ContractOutputType.Signature, ContractOutputTechnicalType.Object, new ArrayList<>());
+    super(ContractOutputType.Signature, ContractOutputTechnicalType.Object, List.of());
   }
 
   @Override
