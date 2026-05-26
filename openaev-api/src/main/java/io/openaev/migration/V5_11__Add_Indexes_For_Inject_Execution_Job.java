393c5a9a513b9c0fd7c2fe4b9aac1f1025d39e60
Guillaume Paris <guillaume.paris@filigran.io>
2026-05-26 09:24:28 +0200

    [backend] chore: fix migration 5_11

--- openaev-api/src/main/java/io/openaev/migration/V5_11__Add_Indexes_For_Inject_Execution_Job.java
+++ openaev-api/src/main/java/io/openaev/migration/V5_11__Add_Indexes_For_Inject_Execution_Job.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V5_10__Add_Indexes_For_Inject_Execution_Job extends BaseJavaMigration {
+public class V5_11__Add_Indexes_For_Inject_Execution_Job extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
