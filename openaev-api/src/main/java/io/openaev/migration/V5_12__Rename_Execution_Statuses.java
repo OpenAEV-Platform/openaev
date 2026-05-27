7d1e52b4a12dffc8799db3e6a4a2f83f59c2c468
Gaetan Santucci <gaetan.santucci@outlook.com>
2026-05-27 08:26:07 +0200

    update flyway versionning

--- openaev-api/src/main/java/io/openaev/migration/V5_12__Rename_Execution_Statuses.java
+++ openaev-api/src/main/java/io/openaev/migration/V5_12__Rename_Execution_Statuses.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V5_10__Rename_Execution_Statuses extends BaseJavaMigration {
+public class V5_12__Rename_Execution_Statuses extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
