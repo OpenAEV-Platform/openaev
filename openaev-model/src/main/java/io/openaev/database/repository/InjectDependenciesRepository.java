e1cf4c44d85a84e72b4fd356f70974ef0c042667
Gabriel PEZÉ <gabriel.peze@filigran.io>
2026-06-02 12:07:12 +0200

    [backend] fix(scenario): spotless (#5208)

--- openaev-model/src/main/java/io/openaev/database/repository/InjectDependenciesRepository.java
+++ openaev-model/src/main/java/io/openaev/database/repository/InjectDependenciesRepository.java
@@ -6,10 +6,8 @@ import io.openaev.database.model.Injector;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
