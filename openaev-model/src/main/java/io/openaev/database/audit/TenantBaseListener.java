c9a2433865db04dca8aff2e2154166637bffff56
Romuald Lemesle <romuald.lemesle@filigran.io>
2026-06-02 10:24:16 +0200

    [backend] fix(catalog): introduce ConnectorEntityId on java code

--- openaev-model/src/main/java/io/openaev/database/audit/TenantBaseListener.java
+++ openaev-model/src/main/java/io/openaev/database/audit/TenantBaseListener.java
@@ -3,22 +3,16 @@ package io.openaev.database.audit;
 import io.openaev.context.TenantContext;
 import io.openaev.database.model.Tenant;
 import io.openaev.database.model.TenantBase;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.PrePersist;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class TenantBaseListener<T extends TenantBase> {
-
-  @Autowired private EntityManager entityManager;
 
   @PrePersist
   private void manageTenant(T entity) {
     if (entity.getTenant() == null) {
-      entity.setTenant(entityManager.getReference(Tenant.class, TenantContext.getCurrentTenant()));
+      entity.setTenant(new Tenant(TenantContext.getCurrentTenant()));
     }
   }
 }
