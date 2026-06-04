cd073f3e56b59e1d81ef68199ab4d8cb8d361a67
Camille Roux <camille.roux@filigran.io>
2026-06-04 10:02:17 +0200

    [frontend] feat(chaining): action selection

--- openaev-front/src/utils/api-types.d.ts
+++ openaev-front/src/utils/api-types.d.ts
@@ -5451,6 +5451,25 @@ export interface PageInjectTestStatusOutput {
   totalPages?: number;
 }
 
+export interface PageInjectorContractBaseOutput {
+  content?: InjectorContractBaseOutput[];
+  empty?: boolean;
+  first?: boolean;
+  last?: boolean;
+  /** @format int32 */
+  number?: number;
+  /** @format int32 */
+  numberOfElements?: number;
+  pageable?: PageableObject;
+  /** @format int32 */
+  size?: number;
+  sort?: SortObject[];
+  /** @format int64 */
+  totalElements?: number;
+  /** @format int32 */
+  totalPages?: number;
+}
+
 export interface PageKillChainPhase {
   content?: KillChainPhase[];
   empty?: boolean;
