2e3133d6f04a39d07b0faf91749d5d7f56e52f6c
Gabriel Pezé <gabriel.peze@filigran.io>
2026-06-05 14:17:02 +0200

    feat(url-access-token): implement url access token management (#5544)

--- openaev-front/src/utils/api-types.d.ts
+++ openaev-front/src/utils/api-types.d.ts
@@ -6361,6 +6361,7 @@ export interface PlatformSettings {
     | "OPENAEV_TRIALS_XTMHUB"
     | "INJECT_CHAINING"
     | "AUDIT_LOG"
+    | "URL_ACCESS_TOKEN"
   )[];
   /** True if the Tanium Executor is enabled */
   executor_tanium_enable?: boolean;
@@ -6632,6 +6633,7 @@ export interface PublicPlatformSettings {
     | "OPENAEV_TRIALS_XTMHUB"
     | "INJECT_CHAINING"
     | "AUDIT_LOG"
+    | "URL_ACCESS_TOKEN"
   )[];
   /** Map of the messages to display on the screen by their level (the level available are DEBUG, INFO, WARN, ERROR, FATAL) */
   platform_banner_by_level?: Record<string, string[]>;
