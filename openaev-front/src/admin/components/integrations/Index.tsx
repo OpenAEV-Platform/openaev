2d760d5d5253ae455c1de9cd9812e08c658d00e8
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-06-08 16:58:12 +0200

    fix some front stuff
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-front/src/admin/components/integrations/Index.tsx
+++ openaev-front/src/admin/components/integrations/Index.tsx
@@ -51,7 +51,7 @@ const Index = () => {
 
           <Route path="secrets-providers" element={errorWrapper(SecretsProviderLayout)()}>
             <Route index element={<ConnectorList />} />
-            <Route path=":secretsProviderId" element={<ConnectorPage />} />
+            <Route path=":secrets_providerId" element={<ConnectorPage />} />
           </Route>
 
           {/* deployed / available tabs of the merged integrations page */}
