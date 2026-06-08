81053ff5a67e55f45e4bd575da80a8d7bba09483
Corinne Krych <corinne.krych@filigran.io>
2026-06-08 16:19:09 +0200

    fix(spotless): generate ui type executor.is_external

--- openaev-api/src/main/java/io/openaev/utils/mapper/ExecutorMapper.java
+++ openaev-api/src/main/java/io/openaev/utils/mapper/ExecutorMapper.java
@@ -32,6 +32,7 @@ public class ExecutorMapper {
         .doc(executor.getDoc())
         .backgroundColor(executor.getBackgroundColor())
         .existing(existingExecutor)
+        .external(executor.isExternal())
         .connectorInstance(
             connectorInstance != null
                 ? connectorInstanceMapper.toConnectorInstanceOutput(connectorInstance)
