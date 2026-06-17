37a71ff40a634b0297ca63905228ce1d4af2b20a
Gaetan Santucci <gaetan.santucci@outlook.com>
2026-06-17 10:52:29 +0200

    fix

--- openaev-api/src/main/java/io/openaev/rest/atomic_testing/AtomicTestingApi.java
+++ openaev-api/src/main/java/io/openaev/rest/atomic_testing/AtomicTestingApi.java
@@ -211,7 +211,7 @@ public class AtomicTestingApi extends RestBehavior {
         injectExpectationService
             .findMergedExpectationsByInjectAndTargetAndTargetType(injectId, targetId, targetType)
             .stream()
-            .sorted(Comparator.comparing(expectation -> expectation.getType().name()))
+            .sorted(Comparator.comparingInt(expectation -> expectation.getType().ordinal()))
             .toList());
   }
 
