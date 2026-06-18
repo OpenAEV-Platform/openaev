093b6715ad3021650870c0980af1ebe36a8d3a5a
Laurent Giovannoni <laurent.giovannoni@filigran.io>
2026-06-18 12:46:01 +0200

    feat(multi-tenancy): scope the import_mappers read endpoint and prove its HTTP isolation (#6212)

--- openaev-api/src/test/java/io/openaev/rest/MapperApiTest.java
+++ openaev-api/src/test/java/io/openaev/rest/MapperApiTest.java
@@ -20,6 +20,7 @@ import io.openaev.rest.scenario.form.InjectsImportTestInput;
 import io.openaev.rest.scenario.response.ImportTestSummary;
 import io.openaev.service.InjectImportService;
 import io.openaev.service.MapperService;
+import io.openaev.utils.TxCtxTestArgumentResolver;
 import io.openaev.utils.fixtures.PaginationFixture;
 import io.openaev.utils.mockMapper.MockMapperUtils;
 import io.openaev.utilstest.RabbitMQTestListener;
@@ -77,7 +78,10 @@ public class MapperApiTest extends IntegrationTest {
     sessionContextField.setAccessible(true);
     sessionContextField.set(mapperApi, objectMapper);
 
-    mvc = MockMvcBuilders.standaloneSetup(mapperApi).build();
+    mvc =
+        MockMvcBuilders.standaloneSetup(mapperApi)
+            .setCustomArgumentResolvers(TxCtxTestArgumentResolver.missing())
+            .build();
   }
 
   // -- SCENARIOS --
