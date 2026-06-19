e07550055aa802b0cfcf645a6c7c8a61d0e3d0d8
Gabriel PEZÉ <gabriel.peze@filigran.io>
2026-06-19 15:06:24 +0200

    feat(url-access-token): fix pr feedbacks (#5544)

--- openaev-api/src/test/java/io/openaev/scheduler/jobs/UrlAccessTokenPurgeJobTest.java
+++ openaev-api/src/test/java/io/openaev/scheduler/jobs/UrlAccessTokenPurgeJobTest.java
@@ -1,10 +1,6 @@
 package io.openaev.scheduler.jobs;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import io.openaev.api.url_access_token.UrlAccessTokenService;
-import io.openaev.service.PreviewFeatureService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,18 +8,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 @DisplayName("UrlAccessTokenPurgeJob")
 class UrlAccessTokenPurgeJobTest {
 
   @Mock private UrlAccessTokenService urlAccessTokenService;
-  @Mock private PreviewFeatureService previewFeatureService;
 
   @InjectMocks private UrlAccessTokenPurgeJob job;
 
   @Test
-  @DisplayName("given_url_access_feature_enabled_should_run_purge")
-  void given_url_access_feature_enabled_should_run_purge() throws Exception {
+  @DisplayName("Given url access token purge job should run purge")
+  void given_url_access_token_purge_job_should_run_purge() throws Exception {
     // Arrange
     when(urlAccessTokenService.purgeExpiredAndRevokedTokens()).thenReturn(2);
 
