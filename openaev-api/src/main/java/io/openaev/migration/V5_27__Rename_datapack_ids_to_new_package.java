84b7b4a7e2eedc913b109494e8fb3387cece2c2e
Gael Leblan <gael.leblan@filigran.io>
2026-06-26 15:35:01 +0200

    feat(multitenancy): transfer older rabbitmq messages to new queues (#3505)

--- openaev-api/src/main/java/io/openaev/migration/V5_27__Rename_datapack_ids_to_new_package.java
+++ openaev-api/src/main/java/io/openaev/migration/V5_27__Rename_datapack_ids_to_new_package.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V5_26__Rename_datapack_ids_to_new_package extends BaseJavaMigration {
+public class V5_27__Rename_datapack_ids_to_new_package extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
