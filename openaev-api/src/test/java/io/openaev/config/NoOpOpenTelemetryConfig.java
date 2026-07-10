64674dda323bbd5ec8ceb18d12ecfe25a739cdc3
Samuel Hassine <samuel.hassine@filigran.io>
2026-07-10 12:35:07 +0200

    feat(telemetry): replace agent deployment metrics with asset coverage metrics (#6645) (#6646)

--- openaev-api/src/test/java/io/openaev/config/NoOpOpenTelemetryConfig.java
+++ openaev-api/src/test/java/io/openaev/config/NoOpOpenTelemetryConfig.java
@@ -1,11 +1,9 @@
 package io.openaev.config;
 
 import io.openaev.database.repository.SettingRepository;
-import io.openaev.executors.ExecutorService;
-import io.openaev.service.AgentService;
 import io.openaev.telemetry.OpenTelemetryConfig;
 import io.openaev.telemetry.metric_collectors.ActionMetricCollector;
-import io.openaev.telemetry.metric_collectors.AgentMetricCollector;
+import io.openaev.telemetry.metric_collectors.AssetMetricCollector;
 import io.openaev.telemetry.metric_collectors.ChainingSafetyPolicyMetricCollector;
 import io.openaev.telemetry.metric_collectors.InventoryMetricCollector;
 import io.openaev.telemetry.metric_collectors.MetricRegistry;
@@ -56,12 +54,8 @@ public class NoOpOpenTelemetryConfig {
   }
 
   @Bean
-  public AgentMetricCollector agentMetricCollector(
-      MetricRegistry metricRegistry,
-      OpenTelemetryConfig openTelemetryConfig,
-      AgentService agentService,
-      ExecutorService executorService) {
-    return Mockito.mock(AgentMetricCollector.class);
+  public AssetMetricCollector assetMetricCollector(MetricRegistry metricRegistry) {
+    return Mockito.mock(AssetMetricCollector.class);
   }
 
   @Bean
