7ab80c23e44a68cfca1f6368fb953ca0b1d1ddd7
Valentin Stern <valentin.stern@filigran.io>
2026-07-16 15:49:50 +0200

    feat: redirect user to autoregister from top bar and scenario deploy

--- openaev-front/src/admin/components/nav/TopBar.tsx
+++ openaev-front/src/admin/components/nav/TopBar.tsx
@@ -316,7 +316,7 @@ const TopBar: FunctionComponent = () => {
                             </Badge>
                           </a>
                         ) : (
-                          <Link className={classes.xtmItem} to="/admin/settings/experience" onClick={handleCloseXtm}>
+                          <Link className={classes.xtmItem} to="/admin/redirect/connect-xtm-hub" onClick={handleCloseXtm}>
                             <Badge variant="dot" color="warning">
                               {xtmhubBadgeImg}
                             </Badge>
