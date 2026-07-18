0ab0483ad0de6466914e6676bd5d288bd1a66dee
Samuel Hassine <samuel.hassine@filigran.io>
2026-07-18 14:03:07 +0200

    fix(dashboard): resolve security domain filter ids to names in filter chips

--- openaev-front/src/components/common/queryable/filter/useRetrieveOptions.tsx
+++ openaev-front/src/components/common/queryable/filter/useRetrieveOptions.tsx
@@ -78,6 +78,7 @@ const useRetrieveOptions = () => {
       case 'action_domains':
       case 'injector_contract_domains':
       case 'inject_contract_domains':
+      case 'base_security_domains_side':
         searchDomainsByIdsAsOption(ids).then((response) => {
           setOptions(response.data);
         });
