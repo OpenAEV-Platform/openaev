8015354144c1c851b39637e99b2f8b9876ea952a
Marine LM <marine.lemezo@filigran.io>
2026-07-20 20:01:04 +0200

    fix(expectations): fix rebase problems
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-front/src/admin/components/common/injects/expectations/Expectation.ts
+++ openaev-front/src/admin/components/common/injects/expectations/Expectation.ts
@@ -22,7 +22,7 @@ export interface ExpectationInput {
   expectation_is_multi_selectable?: boolean;
   // Security platform types expected to fulfil this expectation (empty = any platform).
   expectation_expected_security_platform_types?: string[];
-  expectation_is_predefined?: number;
+  expectation_is_predefined: boolean;
 }
 
 export const SECURITY_PLATFORM_TYPES = [
