6bce2b4fb0e8cbf865ecca2d92d717e07edc7d59
Marine LM <marine.lemezo@filigran.io>
2026-07-27 17:52:17 +0200

    feat(secret-provider): improve code
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-api/src/main/java/io/openaev/secrets/provider/SecretsProvider.java
+++ openaev-api/src/main/java/io/openaev/secrets/provider/SecretsProvider.java
@@ -12,6 +12,8 @@ public abstract class SecretsProvider extends BaseConnectorEntity implements Ten
 
   public static final String SERVICE_NAME = "secrets-provider";
 
+  public static final String SERVICE_NAME = "secrets-provider";
+
   @JsonProperty("secrets_provider_id")
   @Getter
   @Setter
