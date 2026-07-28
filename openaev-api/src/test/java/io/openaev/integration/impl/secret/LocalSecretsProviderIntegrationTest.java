859174127b7b17ff8bc12e705dd50309327548c9
Marine LM <marine.lemezo@filigran.io>
2026-07-28 10:49:02 +0200

    feat(secret-provider): correct test
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

--- openaev-api/src/test/java/io/openaev/integration/impl/secret/LocalSecretsProviderIntegrationTest.java
+++ openaev-api/src/test/java/io/openaev/integration/impl/secret/LocalSecretsProviderIntegrationTest.java
@@ -3,7 +3,6 @@ package io.openaev.integration.impl.secret;
 import static io.openaev.integration.impl.secrets.local.LocalSecretsProviderIntegration.LOCAL_SECRETS_PROVIDER_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.openaev.config.OpenAEVConfig;
 import io.openaev.database.model.ConnectorInstance;
 import io.openaev.database.model.ConnectorType;
 import io.openaev.integration.ComponentRequest;
@@ -23,7 +22,6 @@ import io.openaev.service.connector_instances.NativeEncryptionService;
 import io.openaev.utilstest.RabbitMQTestListener;
 import java.util.Comparator;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -32,7 +30,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {"openaev.enabled-dev-features=CREDENTIAL_ASSET", "spring.cache.type=none"})
 @Transactional
 @TestExecutionListeners(
     value = {RabbitMQTestListener.class},
@@ -47,7 +46,6 @@ public class LocalSecretsProviderIntegrationTest {
   @Autowired private SecretService secretService;
   @Autowired private SecretReferenceService secretReferenceService;
   @Autowired private PreviewFeatureService previewFeatureService;
-  @Autowired private OpenAEVConfig openAEVConfig;
 
   private LocalSecretsProviderIntegrationFactory getFactory() {
     return new LocalSecretsProviderIntegrationFactory(
@@ -59,11 +57,6 @@ public class LocalSecretsProviderIntegrationTest {
         secretService,
         secretReferenceService,
         previewFeatureService);
-  }
-
-  @BeforeEach()
-  void setup() {
-    openAEVConfig.setEnabledDevFeatures("CREDENTIAL_ASSET");
   }
 
   @Nested
