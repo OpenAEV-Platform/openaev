e2e35c4cb61998ee4b9d2bd1cb6d8d977295a5e3
Eva Ezzeribi <eva.ezzeribi@filigran.io>
2026-07-30 16:47:44 +0200

    feat(chaining) : fix review

--- openaev-model/src/main/java/io/openaev/database/repository/attackpath/AttackPathExecutionRemediationRepository.java
+++ openaev-model/src/main/java/io/openaev/database/repository/attackpath/AttackPathExecutionRemediationRepository.java
@@ -1,15 +1,16 @@
 package io.openaev.database.repository.attackpath;
 
 import io.openaev.database.model.attackpath.AttackPathExecutionRemediation;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface AttackPathExecutionRemediationRepository
-    extends CrudRepository<AttackPathExecutionRemediation, String> {
+    extends JpaRepository<AttackPathExecutionRemediation, String> {
 
   List<AttackPathExecutionRemediation> findByStepId(String stepId);
 
