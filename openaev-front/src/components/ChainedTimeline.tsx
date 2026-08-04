879b02536269258bb0ecd8e67ecc3fbe6bf5ad3e
Guillaume Paris <guillaume.paris@filigran.io>
2026-08-04 10:01:38 +0200

    fix(ChainedTimeline): correct formatting and spacing in timeline flow logic

--- openaev-front/src/components/ChainedTimeline.tsx
+++ openaev-front/src/components/ChainedTimeline.tsx
@@ -163,7 +163,7 @@ const ChainedTimelineFlow: FunctionComponent<Props> = ({
     if (startDate !== undefined) {
       startDate = cronObject !== null
         ? moment(startDate).utc().hour(cronObject.getRecurrenceTime().hour || 0).minute(cronObject.getRecurrenceTime().minute || 0)
-            .format()
+          .format()
         : moment(startDate).utc().format();
     }
   } else if (exercise !== undefined) {
@@ -486,9 +486,9 @@ const ChainedTimelineFlow: FunctionComponent<Props> = ({
       const label = startDate === undefined
         ? formatRelativeTime(seconds)
         : (() => {
-            const date = moment.utc(startDate).add(seconds, 's').toDate();
-            return `${fld(date)} - ${ft(date)}`;
-          })();
+          const date = moment.utc(startDate).add(seconds, 's').toDate();
+          return `${fld(date)} - ${ft(date)}`;
+        })();
       setGhost({
         visible: ghostClickable,
         x: eventMove.clientX - rect.left,
@@ -632,7 +632,7 @@ const ChainedTimelineFlow: FunctionComponent<Props> = ({
             onClick={onPlaygroundClick}
             onMouseEnter={showGhost}
             onMouseLeave={hideGhost}
-            onReconnect={() => {}}
+            onReconnect={() => { }}
             // @ts-expect-error the xyflow signature is not well defined here
             onReconnectEnd={onReconnectEnd}
             edgesReconnectable={permissions.canManage}
