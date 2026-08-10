9ab620c5df934e2fcee96f2488bc6742462a4c23
Samuel Hassine <samuel.hassine@filigran.io>
2026-08-10 11:14:11 +0200

    style(phishing): order static test imports for spotless (#7311)

--- openaev-api/src/test/java/io/openaev/injectors/phishing/PhishingExecutorTest.java
+++ openaev-api/src/test/java/io/openaev/injectors/phishing/PhishingExecutorTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import io.openaev.config.OpenAEVConfig;
 import io.openaev.database.model.Execution;
