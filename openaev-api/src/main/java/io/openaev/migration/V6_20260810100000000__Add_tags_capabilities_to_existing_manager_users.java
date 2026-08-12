7fda0c234ab294c9c0e5415928cad826e8d5cf49
johanah29 <johanah.lekeu@filigran.io>
2026-08-12 09:59:12 +0200

    comment
    Signed-off-by: johanah29 <johanah.lekeu@filigran.io>

--- openaev-api/src/main/java/io/openaev/migration/V6_20260810100000000__Add_tags_capabilities_to_existing_manager_users.java
+++ openaev-api/src/main/java/io/openaev/migration/V6_20260810100000000__Add_tags_capabilities_to_existing_manager_users.java
@@ -15,15 +15,16 @@ public class V6_20260810100000000__Add_tags_capabilities_to_existing_manager_use
       statement.execute(
           """
           INSERT INTO roles_capabilities (role_id, capability)
-          SELECT r.role_id, c.capability
-          FROM roles r
+          SELECT DISTINCT rc.role_id, c.capability
+          FROM roles_capabilities rc
+          JOIN roles r ON r.role_id = rc.role_id
           CROSS JOIN (
               VALUES
                 ('ACCESS_TAGS'),
                 ('MANAGE_TAGS'),
                 ('DELETE_TAGS')
           ) AS c(capability)
-          WHERE r.role_name = 'Manager'
+          WHERE rc.capability = 'ACCESS_TENANT_SETTINGS'
             AND r.tenant_id IS NOT NULL
           ON CONFLICT (role_id, capability) DO NOTHING;
           """);
