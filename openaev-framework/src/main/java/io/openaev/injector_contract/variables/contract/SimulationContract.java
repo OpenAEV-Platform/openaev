4ddb4269d81e554931b8728c8aeccab6c3e95f05
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-08-24 16:30:37 +0200

    spotless
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-framework/src/main/java/io/openaev/injector_contract/variables/contract/SimulationContract.java
+++ openaev-framework/src/main/java/io/openaev/injector_contract/variables/contract/SimulationContract.java
@@ -4,11 +4,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.openaev.database.model.Exercise;
 
 public record SimulationContract(
-    @VariableContract(name = VARIABLE_FAMILY + ".id", description = "Id of the simulation in the platform") @JsonProperty("exercise_id")
+    @VariableContract(
+            name = VARIABLE_FAMILY + ".id",
+            description = "Id of the simulation in the platform")
+        @JsonProperty("exercise_id")
         String simulationId,
-    @VariableContract(name = VARIABLE_FAMILY + ".name", description = "Name of the simulation") @JsonProperty("exercise_name")
+    @VariableContract(name = VARIABLE_FAMILY + ".name", description = "Name of the simulation")
+        @JsonProperty("exercise_name")
         String simulationName,
-    @VariableContract(name = VARIABLE_FAMILY + ".description", description = "Description of the simulation") @JsonProperty("exercise_description")
+    @VariableContract(
+            name = VARIABLE_FAMILY + ".description",
+            description = "Description of the simulation")
+        @JsonProperty("exercise_description")
         String simulationDescription) {
   public static final String VARIABLE_FAMILY = "exercise";
 
