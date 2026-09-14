17f17b1c59810c0145e6de2749a58c27d2ca96b1
Corinne Krych <corinne.krych@filigran.io>
2026-09-14 11:39:08 +0200

    chore(marking): rename migration

--- openaev-api/src/main/java/io/openaev/migration/V6_20260914120000000__Add_marking_definitions.java
+++ openaev-api/src/main/java/io/openaev/migration/V6_20260914120000000__Add_marking_definitions.java
@@ -6,7 +6,7 @@ import org.flywaydb.core.api.migration.Context;
 import org.springframework.stereotype.Component;
 
 @Component
-public class V6_20260826120000000__Add_marking_definitions extends BaseJavaMigration {
+public class V6_20260914120000000__Add_marking_definitions extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
