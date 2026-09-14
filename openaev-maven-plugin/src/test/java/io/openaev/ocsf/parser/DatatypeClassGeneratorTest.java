208a82244c582529d470c6223b70c93862c6f05c
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-09-14 10:06:09 +0200

    fix test
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-maven-plugin/src/test/java/io/openaev/ocsf/parser/DatatypeClassGeneratorTest.java
+++ openaev-maven-plugin/src/test/java/io/openaev/ocsf/parser/DatatypeClassGeneratorTest.java
@@ -54,7 +54,7 @@ public class DatatypeClassGeneratorTest {
                   }
 
                   @java.lang.Override
-                  protected boolean validate() {
+                  public boolean validate() {
                     return getValue().matches("^\\\\d{4}-\\\\d{2}-\\\\d{2}[Tt]\\\\d{2}:\\\\d{2}:\\\\d{2}(?:\\\\.\\\\d+)?([Zz]|[\\\\+-]\\\\d{2}:\\\\d{2})?$");
                   }
                 }
