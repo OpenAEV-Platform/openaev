f6c8ff5fafdf2f393e332804991b57316ca75414
Corinne Krych <corinne.krych@filigran.io>
2026-09-14 16:10:53 +0200

    fix(marking): use CrudRepository interface instead of JpaReposotory

--- openaev-model/src/main/java/io/openaev/database/repository/MarkingDefinitionRepository.java
+++ openaev-model/src/main/java/io/openaev/database/repository/MarkingDefinitionRepository.java
@@ -1,15 +1,15 @@
 package io.openaev.database.repository;
 
 import io.openaev.database.model.MarkingDefinition;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MarkingDefinitionRepository
-    extends JpaRepository<MarkingDefinition, String>, JpaSpecificationExecutor<MarkingDefinition> {
+    extends CrudRepository<MarkingDefinition, String>, JpaSpecificationExecutor<MarkingDefinition> {
 
   @Query(
       """
