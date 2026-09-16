80e19effb6e897c97f3bbb39e58ed4e88b849570
Marine LM <marine.lemezo@filigran.io>
2026-09-16 09:29:13 +0200

    feat(credentials): add InjectAuthorisation + injects_secret_references + RBAC ResolveInjectSecret
    Signed-off-by: Marine LM <marine.lemezo@filigran.io>

---
+++ openaev-model/src/main/java/io/openaev/database/model/InjectAuthorisation.java
@@ -0,0 +1,46 @@
+package io.openaev.database.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.openaev.helper.MonoIdDeserializerHelper;
+import io.openaev.helper.MonoIdSerializer;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Setter
+@Getter
+@Entity
+@Table(
+    name = "inject_authorisations",
+    uniqueConstraints =
+        @UniqueConstraint(name = "uk_inject_authorisations_inject_id", columnNames = "inject_id"))
+public class InjectAuthorisation {
+
+  @Id
+  @Column(name = "inject_authorisation_id")
+  @GeneratedValue(generator = "UUID")
+  @UuidGenerator
+  @JsonProperty("inject_authorisation_id")
+  private String id;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "inject_id", nullable = false, unique = true)
+  @JsonSerialize(using = MonoIdSerializer.class)
+  @JsonDeserialize(using = MonoIdDeserializerHelper.class)
+  @JsonProperty("inject_id")
+  @Schema(implementation = String.class)
+  private Inject inject;
+
+  @Column(name = "inject_authorisation_code", nullable = false)
+  @JsonProperty("inject_authorisation_code")
+  private String code;
+
+  @Column(name = "inject_authorisation_issued_at")
+  @JsonProperty("inject_authorisation_issued_at")
+  private Instant issuedAt;
+}
