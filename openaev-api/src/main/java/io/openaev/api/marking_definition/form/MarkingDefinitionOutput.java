b1073e27df3ed35db652803276a59f6e79bad955
johanah29 <johanah.lekeu@filigran.io>
2026-09-17 16:52:37 +0200

    feat(marking): [Task1] revisit front-end marking definitions create form (#7954)
    Signed-off-by: johanah29 <johanah.lekeu@filigran.io>

--- openaev-api/src/main/java/io/openaev/api/marking_definition/form/MarkingDefinitionOutput.java
+++ openaev-api/src/main/java/io/openaev/api/marking_definition/form/MarkingDefinitionOutput.java
@@ -9,7 +9,7 @@ public record MarkingDefinitionOutput(
     @JsonProperty("marking_definition_id") @NotBlank String id,
     @JsonProperty("marking_definition_type") @NotBlank String type,
     @JsonProperty("marking_definition_definition") @NotBlank String definition,
-    @JsonProperty("marking_definition_color") String color,
+    @JsonProperty("marking_definition_color") @NotBlank String color,
     @JsonProperty("marking_definition_order") @NotNull Integer order,
     @JsonProperty("marking_definition_protected") @NotNull Boolean protectedDefinition,
     @JsonProperty("marking_definition_created_at") @NotNull Instant createdAt) {}
