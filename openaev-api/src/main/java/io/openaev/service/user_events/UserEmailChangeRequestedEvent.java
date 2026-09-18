0b7c7ea77774f0bbda3f746fb4a68788166320c5
Antoine MAZEAS <antoine.mazeas@filigran.io>
2026-09-18 15:19:41 +0200

    use in-memory store for confirmation codes
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- openaev-api/src/main/java/io/openaev/service/user_events/UserEmailChangeRequestedEvent.java
+++ openaev-api/src/main/java/io/openaev/service/user_events/UserEmailChangeRequestedEvent.java
@@ -3,4 +3,4 @@ package io.openaev.service.user_events;
 import io.openaev.database.model.User;
 
 /** Event published when a user has requested changing their account email. */
-public record UserEmailChangeRequestedEvent(User user, String newEmail) {}
+public record UserEmailChangeRequestedEvent(User user, String confirmationCode, String newEmail) {}
